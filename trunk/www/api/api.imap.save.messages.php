9c13412a089c7427bfb7b830e1950a03c31a3306
revue-de-presse <contact+github@revue-de-presse.org>
2011-03-05 10:35:57 +0000

    20110305 :: support :: api :: impa ::
    Add uid to header properties
    
    (trunk :: revision :: 88)
    
    git-svn-id: svn://127.0.0.1@88 c738b8bc-0dc4-4482-b3d8-545fa36257ac

--- trunk/www/api/api.imap.save.messages.php
+++ trunk/www/api/api.imap.save.messages.php
@@ -6,10 +6,10 @@
 *************
 * 2011 03 05
 *************
-* 
-* Prepare loading of latest messages only
 *
-* (trunk :: revision :: 84)
+* Add uid to header properties
+*
+* (trunk :: revision :: 88)
 *
 */
 
@@ -22,70 +22,20 @@ $class_dumper = $class_application::getDumperClass();
 
 $results = $class_header::getImapSearchResults( 'SUBJECT "slashdot"' );
 
-$mailbox =
-
-$resource = NULL;
-
-$_messages = array();
-
-if ( is_null( $mailbox ) )
-
-	$mailbox = self::getImapMailbox();
-
-if ( is_null( $resource ) )
-
-	$resource = self::openImapStreap( $mailbox );
-
-while ( list( $label, $uids ) = each( $results ) )
-{
-	while (
-		( list( , $uid ) = each( $uids ))
-	)
-	{
-		if ( $uid > 110213 )
-		{
-			imap_reopen( $resource, $label );
-	
-			$_messages[ $uid ] = array(
-				PROPERTY_BODY =>
-					imap_fetchbody( $resource, $uid, '1', FT_UID ),
-				PROPERTY_HEADER =>
-					imap_fetchheader( $resource, $uid, FT_UID ),
-				PROPERTY_STRUCTURE =>
-					imap_fetchstructure( $resource, $uid, FT_UID )
-			);
-		}
-	}
-
-	reset( $uids );
-}
+$messages = $class_header::getImapMessages( $results );
 
 if ( is_array( $messages )  && count( $messages ) )
 {
-	end( $messages );
-	list( $uid ) = each( $messages );
-
-	echo $uid;
-
-	$class_dumper::log(
-		__METHOD__,
-		array(
-			'[latest uid]',
+	foreach ( $messages as $uid => $properties )
+	{
+		$header = $class_header::make(
+			$properties[PROPERTY_HEADER],
 			$uid
-		),
-		TRUE
-	);	
-
-	//foreach ( $messages as $uid => $properties )
-	//{
-	//	$header = $class_header::make(
-	//		$properties[PROPERTY_HEADER],
-	//		$uid
-	//	);
-	//
-	//	$message = $class_message::make(
-	//		$properties[PROPERTY_BODY],
-	//		$header->{PROPERTY_ID}
-	//	);
-	//}
+		);
+	
+		$message = $class_message::make(
+			$properties[PROPERTY_BODY],
+			$header->{PROPERTY_ID}
+		);
+	}
 }
