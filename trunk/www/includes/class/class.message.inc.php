892d225c74703e0e8ee0ae97627c97638cfaa201
revue-de-presse <contact+github@revue-de-presse.org>
2011-03-05 21:01:18 +0000

    20110305 :: support :: api :: imap ::
    Implement the message importation methods for IMAP with gmail accounts
    
    git-svn-id: svn://127.0.0.1@110 c738b8bc-0dc4-4482-b3d8-545fa36257ac

--- trunk/www/includes/class/class.message.inc.php
+++ trunk/www/includes/class/class.message.inc.php
@@ -181,14 +181,14 @@ class Message extends Header
 						$body = imap_body( $resource, $uid, FT_UID ),
 						$header->{PROPERTY_ID}
 					);
-					
-					echo
-						'[uid :: ', $uid, ']<br /><br/>',
-						'[header]', '<br /><br/>',
-						'<pre>', $header_value, '</pre>', '<br/><br/>',
-						'[body]', '<br /><br/>',
-						'<pre>', $body, '</pre>', '<br/><br/><br/><br/>'
-					;
+
+					//echo
+					//	'[uid :: ', $uid, ']<br /><br/>',
+					//	'[header]', '<br /><br/>',
+					//	'<pre>', $header_value, '</pre>', '<br/><br/>',
+					//	'[body]', '<br /><br/>',
+					//	'<pre>', $body, '</pre>', '<br/><br/><br/><br/>'
+					//;
 
 					ob_end_flush();
 				}
