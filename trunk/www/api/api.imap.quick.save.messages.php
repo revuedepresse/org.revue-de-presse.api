91d37682da7a9af60f764310c6e126e5f6abfcb4
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2011-03-05 11:18:10 +0000

    20110305 :: support :: api :: imap
    Start revising the message saving methods from the IMAP API for optimizing
    
    (trunk :: revision :: 89)
    
    git-svn-id: svn://127.0.0.1@91 c738b8bc-0dc4-4482-b3d8-545fa36257ac

--- trunk/www/api/api.imap.quick.save.messages.php
+++ trunk/www/api/api.imap.quick.save.messages.php
@@ -21,72 +21,106 @@ $class_message = $class_application::getMessageClass();
 
 $class_dumper = $class_application::getDumperClass();
 
-$results = $class_header::getImapSearchResults( 'SUBJECT "slashdot"' );
+$criteria = 'SUBJECT "slashdot"';
 
 $mailbox =
 
 $resource = NULL;
 
-$_messages = array();
+$_messages =
 
-if ( is_null( $mailbox ) )
+$search_results = array();
+
+if ( is_null( $mailbox ) && is_null( $resource ) )
 
 	$mailbox = self::getImapMailbox();
 
 if ( is_null( $resource ) )
 
-	$resource = self::openImapStreap( $mailbox );
+	$resource = self::openImapStream( $mailbox );
 
-while ( list( $label, $uids ) = each( $results ) )
+if ( is_null( $labels ) )
+
+	$labels = array( $mailbox.'[Gmail]/All Mail' );
+
+while ( list( , $label ) = each( $labels ) )
 {
-	while (
-		( list( , $uid ) = each( $uids ))
-	)
-	{
-		if ( $uid > 110213 )
-		{
-			imap_reopen( $resource, $label );
-	
-			$_messages[ $uid ] = array(
-				PROPERTY_BODY =>
-					imap_fetchbody( $resource, $uid, '1', FT_UID ),
-				PROPERTY_HEADER =>
-					imap_fetchheader( $resource, $uid, FT_UID ),
-				PROPERTY_STRUCTURE =>
-					imap_fetchstructure( $resource, $uid, FT_UID )
-			);
-		}
-	}
+	imap_reopen( $resource, $label );			
 
-	reset( $uids );
+	$search_results[$label]  =
+		imap_search( $resource, $criteria, SE_UID )
+	;
 }
 
-if ( is_array( $messages )  && count( $messages ) )
-{
-	end( $messages );
-	list( $uid ) = each( $messages );
+list( , $uids ) = each( $search_results );
 
-	echo $uid;
+end( $uids );
 
-	$class_dumper::log(
-		__METHOD__,
-		array(
-			'[latest uid]',
-			$uid
-		),
-		TRUE
-	);	
+list( , $uid ) = each( $uids );
 
-	//foreach ( $messages as $uid => $properties )
-	//{
-	//	$header = $class_header::make(
-	//		$properties[PROPERTY_HEADER],
-	//		$uid
-	//	);
-	//
-	//	$message = $class_message::make(
-	//		$properties[PROPERTY_BODY],
-	//		$header->{PROPERTY_ID}
-	//	);
-	//}
-}
+if ( $verbose )
+
+	echo 'last uid: ', $uid;
+
+$class_dumper::log(
+	__METHOD__,
+	array(
+		'[last uid]',
+		$uid
+	),
+	$verbose
+);
+
+//while ( list( $label, $uids ) = each( $search_results ) )
+//{
+//	while (
+//		( list( , $uid ) = each( $uids ))
+//	)
+//	{
+//		if ( $uid > 110213 )
+//		{
+//			imap_reopen( $resource, $label );
+//	
+//			$_messages[ $uid ] = array(
+//				PROPERTY_BODY =>
+//					imap_fetchbody( $resource, $uid, '1', FT_UID ),
+//				PROPERTY_HEADER =>
+//					imap_fetchheader( $resource, $uid, FT_UID ),
+//				PROPERTY_STRUCTURE =>
+//					imap_fetchstructure( $resource, $uid, FT_UID )
+//			);
+//		}
+//	}
+//
+//	reset( $uids );
+//}
+//
+//if ( is_array( $messages )  && count( $messages ) )
+//{
+//	end( $messages );
+//	list( $uid ) = each( $messages );
+//
+//	echo $uid;
+//
+//	$class_dumper::log(
+//		__METHOD__,
+//		array(
+//			'[latest uid]',
+//			$uid
+//		),
+//		TRUE
+//	);	
+//
+//	foreach ( $messages as $uid => $properties )
+//	{
+//		$header = $class_header::make(
+//			$properties[PROPERTY_HEADER],
+//			$uid
+//		);
+//	
+//		$message = $class_message::make(
+//			$properties[PROPERTY_BODY],
+//			$header->{PROPERTY_ID}
+//		);
+//	}
+//}
