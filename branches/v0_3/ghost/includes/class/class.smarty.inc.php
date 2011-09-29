3e5e300a88b56b593cf8cbae7d7f45f0def5644b
revue-de-presse <contact+github@revue-de-presse.org>
2011-09-29 22:31:04 +0000

    Removed branches/v0_3
    git-svn-id: svn://127.0.0.1@363 c738b8bc-0dc4-4482-b3d8-545fa36257ac

--- branches/v0_3/ghost/includes/class/class.smarty.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once(
-	dirname(__FILE__).
-		DIR_PARENT_DIRECTORY.
-			DIR_PARENT_DIRECTORY.
-				SCRIPT_LIBRARY_SMARTY
-);
--- branches/v0_3/ghost/templates/block.footer.tpl
+++
@@ -1,4 +0,0 @@
-<div id="div_view_feedback">
-   <a href="#send_feedback">Feedback</a>
-   <div id='view_feedback_dialog'></div>
-</div>
