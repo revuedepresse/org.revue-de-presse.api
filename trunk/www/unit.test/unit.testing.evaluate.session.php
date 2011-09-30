fdd9dc0f9b9ce55cab9340f0b2c99769214a4613
revue-de-presse <contact+github@revue-de-presse.org>
2011-09-30 07:57:50 +0000

    git-svn-id: svn://127.0.0.1@366 c738b8bc-0dc4-4482-b3d8-545fa36257ac

--- trunk/www/unit.test/unit.testing.evaluate.session.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once(dirname(__FILE__).'/../includes/sefi.globals.php');
-
-fprint( $_SESSION, $verbose_mode );
-
-$firephp = FirePHP::getInstance( TRUE );
-$firephp->log( $_SESSION );
--- trunk/www/unit.test/unit.testing.save.author.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$class_author = $class_application::getAuthorClass();
-
-$class_author::saveAuthor();
