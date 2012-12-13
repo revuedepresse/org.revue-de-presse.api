97082488e4e4679e58a8057e2dabaaaee687ec31
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2012-12-13 21:37:22 +0000

    git-svn-id: svn://127.0.0.1@560 c738b8bc-0dc4-4482-b3d8-545fa36257ac

--- trunk/www/api/lib/Symfony/app/bootstrap.test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$legacyBootstrap = '## FILL ABSOLUTE PATH ##';
-
-if (file_exists($legacyBootstrap)) {
-    require($legacyBootstrap);
-}
-
-require('bootstrap.php.cache');
--- trunk/www/unit.testing/unit.testing.translate.string.to.method.name.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$class_parser = CLASS_PARSER;
-
-$class_dumper::log(
-    __METHOD__,
-    array($class_parser::translate_entity('get.type', ENTITY_NAME_METHOD)),
-    true
-);
