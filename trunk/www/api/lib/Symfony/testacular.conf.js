dfbfcecafc2d8676c31475228b783f6198094920
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2013-02-16 13:36:58 +0000

    [unit testing] Revises testacular configuration
    git-svn-id: svn://127.0.0.1@696 c738b8bc-0dc4-4482-b3d8-545fa36257ac

--- trunk/www/api/lib/Symfony/testacular.conf.js
+++ trunk/www/api/lib/Symfony/testacular.conf.js
@@ -59,7 +59,7 @@ autoWatch = false;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['PhantomJS','Chrome','Firefox'];
+browsers = ['PhantomJS'];
 
 
 // If browser does not capture in given timeout [ms], kill it
