e43879807290867c35766f88783c04a8ae09020b
revue-de-presse <contact+github@revue-de-presse.org>
2013-03-15 23:17:22 +0000

    [github repositories] Shows starred repositories with AngularJs
    git-svn-id: svn://127.0.0.1@782 c738b8bc-0dc4-4482-b3d8-545fa36257ac

--- branches/master/api/lib/Symfony/src/WTW/DashboardBundle/Resources/public/wtw/js/app.js
+++ branches/master/api/lib/Symfony/src/WTW/DashboardBundle/Resources/public/wtw/js/app.js
@@ -1,10 +1,12 @@
 'use strict';
 
+angular.module('WTW', ['WTW.filters']).config(
+    ['$routeProvider', function($routeProvider) {
+        var templatesDir = '/sf2/bundles/wtwdashboard/wtw/partials/';
 
-// Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+        $routeProvider.when('/github-repositories', {
+            templateUrl: templatesDir + '/repositories.html',
+            controller: ShowRepositoriesAction
+        });
+        $routeProvider.otherwise({redirectTo: '/github-repositories'});
+    }]);
