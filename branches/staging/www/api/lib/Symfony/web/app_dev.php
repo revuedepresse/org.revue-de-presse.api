9b8670555dc61a10a7f4c9813b0c9f46bba3a29a
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2013-03-16 15:47:15 +0000

    [security] Grants access to development environment to babylon-crusader
    git-svn-id: svn://127.0.0.1@797 c738b8bc-0dc4-4482-b3d8-545fa36257ac

--- branches/staging/www/api/lib/Symfony/web/app_dev.php
+++ branches/staging/www/api/lib/Symfony/web/app_dev.php
@@ -8,15 +8,18 @@ use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(
-        @$_SERVER['REMOTE_ADDR'],
-        array(
-            '127.0.0.1',
-            '::1',
+if (
+    (
+        isset($_SERVER['HTTP_CLIENT_IP'])
+        || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+        || !in_array(
+            $_SERVER['REMOTE_ADDR'],
+            array(
+                '127.0.0.1',
+                '::1',
+            )
         )
-    )
+    ) && ( $_SERVER['HTTP_X_FORWARDED_FOR'] !== '## FILL IP ADDRESS ##' ) 
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
