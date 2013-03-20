bb51cb64406fe96d88f01d52526a3ab4220bba85
revue-de-presse <contact+github@revue-de-presse.org>
2013-03-20 21:05:47 +0000

    [dashboad][routing] Makes routes relative
    git-svn-id: svn://127.0.0.1@822 c738b8bc-0dc4-4482-b3d8-545fa36257ac

--- trunk/www/api/lib/Symfony/src/WTW/DashboardBundle/Resources/public/wtw/js/controllers.js
+++ trunk/www/api/lib/Symfony/src/WTW/DashboardBundle/Resources/public/wtw/js/controllers.js
@@ -3,7 +3,7 @@
 /* Controllers */
 
 function ShowRepositoriesAction($scope, $http) {
-    var getRepositoriesUrl = Routing.generate('get_repositories', {}, true);
+    var getRepositoriesUrl = Routing.generate('get_repositories', {});
 
     $http.get(getRepositoriesUrl).success(function (data) {
         $scope.dashboard = data[0]
