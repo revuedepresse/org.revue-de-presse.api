2b4a00973e969f48b394054cdf3ce846b922c229
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2013-04-21 21:19:25 +0200

    [dashboard] Refactors show documents controller

--- api/lib/Symfony/src/WTW/DashboardBundle/Controller/DocumentController.php
+++ api/lib/Symfony/src/WTW/DashboardBundle/Controller/DocumentController.php
@@ -29,78 +29,26 @@ class DocumentController extends Controller
          */
         $error = null;
         $request = $this->get('request');
-        $results = [];
         $translator = $this->get('translator');
-        $query = new \stdClass;
-        $query->error = null;
-
-        if ($request->request->has('query')) {
-            $query->sql = $request->request->get('query');
-        } else {
-            $query= $this->getDefaultQuery();
-        }
 
         /**
          * @var $connection Connection
          */
         $connection = $this->get('dashboard.dbal_connection');
 
-        if ($connection->idempotentQuery($query->sql)) {
-            try {
-                if ($connection->pdoSafe($query->sql)) {
-                    $results = $connection->delegateQueryExecution($query->sql);
-                } else {
-                    $results = $connection->connect()->execute($query->sql)->fetchResults();
-                }
-            } catch (\Exception $exception) {
-                $query->error = $exception->getMessage();
-            }
+        if ($request->request->has('query')) {
+            $sql = $request->get('query');
         } else {
-            $results = [$translator->trans('requirement_valid_query', array(), 'messages')];
+            $sql = $connection->getDefaultQuery();
         }
+
+        $query = $connection->executeQuery($sql);
 
         return $this->render(
             'WTWDashboardBundle:Document:showDocuments.html.twig', array(
                 'error' => $query->error,
                 'default_query' => $query->sql,
-                'results' => $results,
+                'records' => $query->records,
                 'title' => $translator->trans('title_documents')));
-    }
-
-    /**
-     * @return string
-     */
-    public function getDefaultQuery()
-    {
-        /**
-         * @var $connection Connection
-         */
-        $connection = $this->get('dashboard.dbal_connection');
-        $database = $this->container->getParameter('database_name');
-        $defaultQuery = new \stdClass();
-        $defaultQuery->sql = 'invalid query';
-        $defaultQuery->error = null;
-
-        $baseQuery = <<< QUERY
-SELECT per_value AS query
-FROM {database}weaving_perspective
-WHERE per_type = {type}
-LIMIT 1
-QUERY;
-
-        $query = strtr($baseQuery, array(
-            '{database}' => $database . '.',
-            '{type}' => $connection::QUERY_TYPE_DEFAULT));
-
-        try {
-            $results = $connection->connect()->execute($query)->fetchResults();
-            if (count($results) > 0) {
-                $defaultQuery->sql = $results[0]['query'];
-            }
-        } catch (\Exception $exception) {
-            $defaultQuery->error = $exception->getMessage();
-        }
-
-        return $defaultQuery;
     }
 }
