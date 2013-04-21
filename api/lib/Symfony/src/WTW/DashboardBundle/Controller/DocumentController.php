98639209aebfd8f30de5a0e10a68a3ae78445d15
revue-de-presse <contact+github@revue-de-presse.org>
2013-04-21 17:08:58 +0200

    [dashboard] Use msyqli fallback when Doctrine cannot execute queries containing variables using PDO for MySQL

--- api/lib/Symfony/src/WTW/DashboardBundle/Controller/DocumentController.php
+++ api/lib/Symfony/src/WTW/DashboardBundle/Controller/DocumentController.php
@@ -2,10 +2,10 @@
 
 namespace WTW\DashboardBundle\Controller;
 
-use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration as Extra;
 use Symfony\Component\HttpFoundation\Request;
+use WTW\DashboardBundle\DBAL\Connection;
 
 
 /**
@@ -17,67 +17,56 @@ use Symfony\Component\HttpFoundation\Request;
 class DocumentController extends Controller
 {
     /**
-     * @param null $query
-     *
-     * @Extra\Route("/documents/{query}", name="wtw_dashboard_show_documents")
-     * @Extra\Method("GET|POST")
+     * @Extra\Route("/documents", name="wtw_dashboard_show_documents")
+     * @Extra\Method({"GET", "POST"})
      *
      * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
      */
-    public function showAction($query = null)
+    public function showAction()
     {
         /**
          * @var $request Request
          */
         $request = $this->get('request');
+        $translator = $this->get('translator');
         $query = $request->request->get('query');
 
-        $translator = $this->get('translator');
-        $validQuery = $this->validateQuery($query);
+        /**
+         * @var $connection Connection
+         */
+        $connection = $this->get('dashboard.dbal_connection');
 
-        if ($validQuery) {
-            /**
-             * @var $em EntityManager
-             */
-            $em = $this->get('doctrine.orm.entitymanager');
-            $stmt = $em->getConnection()->prepare($query);
-            $stmt->execute();
-
-            $rows[] = $stmt->fetchAll();
+        if ($connection->idempotentQuery($query)) {
+            if ($connection->pdoSafe($query)) {
+                $entityManager = $this->get('doctrine.orm.entity_manager');
+                $results = $connection->setEntityManager($entityManager)
+                    ->delegateQueryExecution($query);
+            } else {
+                $results = $connection->connect()->execute($query)->fetchResults();
+            }
         } else {
-$query = <<< QUERY
-SELECT id, message
-FROM weaving_preprod.weaving_facebook_status
-LIMIT 0,100
-QUERY;
-
-            $rows = ['single scalar result'];
-            $rows[] = $translator->trans('requirement_valid_query', array(), 'messages');
+            $query = $this->getDefaultQuery();
+            $results = ['single scalar result',
+                $translator->trans('requirement_valid_query', array(), 'messages')];
         }
 
         return $this->render(
             'WTWDashboardBundle:Document:showDocuments.html.twig', array(
-                'default_query' => $query
-                ,
-                'rows' => $rows,
+                'default_query' => $query,
+                'results' => $results,
                 'title' => $translator->trans('title_documents')));
     }
 
     /**
-     * @param $query
-     *
-     * @return bool
+     * @return string
      */
-    public function validateQuery($query)
+    public function getDefaultQuery()
     {
-        return
-            (strlen($query) > 0) &&
-            (false === strpos(strtolower($query), 'update')) &&
-            (false === strpos(strtolower($query), 'insert')) &&
-            (false === strpos(strtolower($query), 'delete')) &&
-            (false === strpos(strtolower($query), 'truncate')) &&
-            (false === strpos(strtolower($query), 'drop')) &&
-            (false === strpos(strtolower($query), 'alter')) &&
-            (false === strpos(strtolower($query), 'grant'));
+        return <<< QUERY
+SELECT id, message
+FROM weaving_preprod.weaving_facebook_status
+LIMIT 0,100
+QUERY;
     }
 }
