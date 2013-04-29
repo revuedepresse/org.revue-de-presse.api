413ba60b1e38a6720cdba57f07ad1d0f4fc0ac7a
revue-de-presse <contact+github@revue-de-presse.org>
2013-04-29 20:46:06 +0200

    [migration] Adds name column to perspective table

---
+++ api/lib/Symfony/app/DoctrineMigrations/Version20130427094639.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your need!
+ */
+class Version20130427094639 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE weaving_perspective ADD per_name VARCHAR(255) DEFAULT NULL");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+
+        $this->addSql("ALTER TABLE weaving_perspective DROP per_name");
+    }
+}
