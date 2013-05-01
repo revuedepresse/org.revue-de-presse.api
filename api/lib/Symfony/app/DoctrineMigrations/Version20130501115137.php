2b0a59fe92576a12ca1ca9a774a0e93637da1ba1
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2013-05-01 12:03:05 +0200

    [migrations] Rewrites migrations

---
+++ api/lib/Symfony/app/DoctrineMigrations/Version20130501115137.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your need!
+ */
+class Version20130501115137 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE weaving_user CHANGE usr_first_name usr_first_name VARCHAR(255) DEFAULT NULL");
+        $this->addSql("ALTER TABLE weaving_privilege CHANGE prv_id prv_id TINYINT(1) NOT NULL");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE weaving_privilege CHANGE prv_id prv_id TINYINT(1) NOT NULL");
+        $this->addSql("ALTER TABLE weaving_user CHANGE usr_first_name usr_first_name VARCHAR(255) NOT NULL");
+    }
+}
--- api/lib/Symfony/app/DoctrineMigrations/Version20130501115138.php
+++ api/lib/Symfony/app/DoctrineMigrations/Version20130501115138.php
@@ -8,12 +8,14 @@ use Doctrine\DBAL\Migrations\AbstractMigration,
 /**
  * Auto-generated Migration: Please modify to your need!
  */
-class Version20130430084515 extends AbstractMigration
+class Version20130501115138 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
 
+        $this->addSql("ALTER TABLE weaving_perspective DROP per_description, CHANGE per_status per_status INT NOT NULL, CHANGE per_type per_type INT NOT NULL, CHANGE per_value per_value LONGTEXT NOT NULL, CHANGE per_date_creation per_date_creation DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, CHANGE per_date_update per_date_update DATETIME DEFAULT '0000-00-00 00:00:00' NOT NULL");
+        $this->addSql("ALTER TABLE weaving_perspective ADD per_name VARCHAR(255) DEFAULT NULL");
         $this->addSql("ALTER TABLE weaving_user CHANGE grp_id grp_id INT DEFAULT NULL, CHANGE usr_status usr_status TINYINT(1) DEFAULT NULL, CHANGE usr_avatar usr_avatar INT DEFAULT NULL, CHANGE usr_last_name usr_last_name VARCHAR(255) DEFAULT NULL");
     }
 
@@ -21,6 +23,8 @@ class Version20130430084515 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
 
+        $this->addSql("ALTER TABLE weaving_perspective DROP per_name");
+        $this->addSql("ALTER TABLE weaving_perspective ADD per_description LONGTEXT DEFAULT NULL, CHANGE per_status per_status INT DEFAULT NULL, CHANGE per_type per_type INT DEFAULT NULL, CHANGE per_value per_value LONGTEXT DEFAULT NULL, CHANGE per_date_creation per_date_creation DATETIME DEFAULT NULL, CHANGE per_date_update per_date_update DATETIME DEFAULT NULL");
         $this->addSql("ALTER TABLE weaving_user CHANGE grp_id grp_id INT NOT NULL, CHANGE usr_status usr_status TINYINT(1) NOT NULL, CHANGE usr_avatar usr_avatar INT NOT NULL, CHANGE usr_last_name usr_last_name VARCHAR(255) NOT NULL");
     }
 }
