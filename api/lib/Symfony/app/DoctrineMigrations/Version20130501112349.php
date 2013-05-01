369c080610d3e59d28e9b3055461c90c9772fadd
revue-de-presse <contact+github@revue-de-presse.org>
2013-05-01 12:03:05 +0200

    [migrations] Rewrites migrations

--- api/lib/Symfony/app/DoctrineMigrations/Version20130501112349.php
+++ api/lib/Symfony/app/DoctrineMigrations/Version20130501112349.php
@@ -8,19 +8,21 @@ use Doctrine\DBAL\Migrations\AbstractMigration,
 /**
  * Auto-generated Migration: Please modify to your need!
  */
-class Version20130429223018 extends AbstractMigration
+class Version20130501112349 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
+        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
 
-        $this->addSql("ALTER TABLE weaving_user ADD usr_twitter_id INT NOT NULL, ADD usr_twitter_username VARCHAR(255) NOT NULL");
+        $parts = explode('\\', __CLASS__);
+        $version = $parts[count($parts) - 1];
+        $fixtures = file_get_contents(__DIR__ . '/fixtures/fixtures_' . $version . '.sql');
+        $this->addSql($fixtures);
     }
 
     public function down(Schema $schema)
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
-
-        $this->addSql("ALTER TABLE weaving_user DROP usr_twitter_id, DROP usr_twitter_username");
     }
 }
