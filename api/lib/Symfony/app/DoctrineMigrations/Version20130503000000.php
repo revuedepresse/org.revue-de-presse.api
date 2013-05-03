1b973dbe9e7247d8b606d0751daa20ad5ec3b859
revue-de-presse <contact+github@revue-de-presse.org>
2013-05-03 03:12:51 +0200

    [dashboard][fixtures] Loads perspectives fixtures

---
+++ api/lib/Symfony/app/DoctrineMigrations/Version20130503000000.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * User: Thierry Marianne
+ * Date: 5/3/13
+ * Time: 3:10 AM
+ */
+
+namespace Application\Migrations;
+
+/**
+ * Class Version20130503000000
+ *
+ * @package Application\Migrations
+ */
+class Version20130503000000
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+
+        $parts = explode('\\', __CLASS__);
+        $version = $parts[count($parts) - 1];
+        $fixtures = file_get_contents(__DIR__ . '/fixtures/fixtures_' . $version . '.sql');
+        $this->addSql($fixtures);
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+    }
+}
