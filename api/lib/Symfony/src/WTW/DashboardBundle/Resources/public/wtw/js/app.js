07bdfcfdcf3d4aa0c6da4fe507752f1059ac04b6
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2013-05-07 23:12:18 +0200

    [isolation] Prevents bootstrap of angular application when the latter is not needed

--- api/lib/Symfony/src/WTW/DashboardBundle/Resources/public/wtw/js/app.js
+++ api/lib/Symfony/src/WTW/DashboardBundle/Resources/public/wtw/js/app.js
@@ -1,10 +1,12 @@
 'use strict';
 
-angular.module('WTW', ['WTW.filters']).config(
-    ['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/github-repositories', {
-            templateUrl: partials + 'repositories.html',
-            controller: ShowRepositoriesAction
-        });
-        $routeProvider.otherwise({redirectTo: '/github-repositories'});
-    }]);
+if (typeof partials !== 'undefined') {
+    angular.module('WTW', ['WTW.filters']).config(
+        ['$routeProvider', function($routeProvider) {
+            $routeProvider.when('/github-repositories', {
+                templateUrl: partials + 'repositories.html',
+                controller: ShowRepositoriesAction
+            });
+            $routeProvider.otherwise({redirectTo: '/github-repositories'});
+        }]);
+}
