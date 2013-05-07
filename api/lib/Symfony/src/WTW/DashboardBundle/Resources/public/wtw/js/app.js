1ab190b2af67dfc9c566afa2f428c9ee51d71c06
revue-de-presse <contact+github@revue-de-presse.org>
2013-05-07 23:55:45 +0200

    [assets] Compiles assets to prevent conflict in production environment

--- api/lib/Symfony/src/WTW/DashboardBundle/Resources/public/wtw/js/app.js
+++ api/lib/Symfony/src/WTW/DashboardBundle/Resources/public/wtw/js/app.js
@@ -1,12 +1,10 @@
 'use strict';
 
-if (typeof partials !== 'undefined') {
-    angular.module('WTW', ['WTW.filters']).config(
-        ['$routeProvider', function($routeProvider) {
-            $routeProvider.when('/github-repositories', {
-                templateUrl: partials + 'repositories.html',
-                controller: ShowRepositoriesAction
-            });
-            $routeProvider.otherwise({redirectTo: '/github-repositories'});
-        }]);
-}
+angular.module('WTW', ['WTW.filters']).config(
+    ['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/github-repositories', {
+            templateUrl: partials + 'repositories.html',
+            controller: ShowRepositoriesAction
+        });
+        $routeProvider.otherwise({redirectTo: '/github-repositories'});
+    }]);
