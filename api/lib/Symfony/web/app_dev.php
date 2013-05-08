2c415605042c467d30256ba785342e7e58c44665
revue-de-presse <contact+github@revue-de-presse.org>
2013-05-08 20:22:53 +0200

    [environment] Revises front controllers settings

--- api/lib/Symfony/web/app_dev.php
+++ api/lib/Symfony/web/app_dev.php
@@ -25,11 +25,10 @@ if (
     exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
 }
 
-$loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
+$loader = require_once __DIR__ . '/../app/autoload.php';
 require_once __DIR__ . '/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
 $request  = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
