f99e2558d109d453c84db9be1137ca2b62391ef3
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2013-05-11 00:44:31 +0200

    [environment settings] Revises cache configuration

--- api/lib/Symfony/web/app_dev.php
+++ api/lib/Symfony/web/app_dev.php
@@ -11,29 +11,28 @@ use Symfony\Component\HttpFoundation\Request;
 if (
     (
         isset($_SERVER['HTTP_CLIENT_IP']) ||
-        isset($_SERVER['HTTP_X_FORWARDED_FOR']) &&
-        (
-            ( $_SERVER['HTTP_X_FORWARDED_FOR']) !== '127.0.0.1' ||
-            ( $_SERVER['HTTP_X_FORWARDED_HOST'] !== '## FILL HOSTNAME ##' )
-        ) && !in_array(
+        !in_array(
                 $_SERVER['REMOTE_ADDR'],
                 array(
                     '127.0.0.1',
                     '::1',
                 )
             )
-    ) && ( $_SERVER['HTTP_X_FORWARDED_FOR'] !== '## FILL IP ADDRESS ##' )
+    ) && (
+        ! isset($_SERVER['HTTP_X_FORWARDED_FOR']) ||
+        ($_SERVER['HTTP_X_FORWARDED_FOR'] !== '## FILL IP ADDRESS ##')
+    )
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
 }
 
-$loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
-//$loader = require_once __DIR__ . '/../app/autoload.php.cache';
+//$loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
+$loader = require_once __DIR__ . '/../app/autoload.php';
 require_once __DIR__ . '/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
+//$kernel->loadClassCache();
 $request  = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
