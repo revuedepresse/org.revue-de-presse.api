297d2e793e0d0e5f73f2377eaaddcf58b32115c0
revue-de-presse <contact+github@revue-de-presse.org>
2013-05-11 18:02:25 +0200

    [profiling] Creates profiling environment

---
+++ api/lib/Symfony/web/app_prof.php
@@ -0,0 +1,34 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+xhprof_enable();
+
+$loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
+
+require_once __DIR__ . '/../app/AppKernel.php';
+
+$kernel = new AppKernel('prof', true);
+$kernel->loadClassCache();
+$request  = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
+
+$xhprofData = xhprof_disable();
+$xhprofRoot = '## FILL ABSOLUTE PATH ##'';
+
+include_once $xhprofRoot . "/xhprof_lib/utils/xhprof_lib.php";
+include_once $xhprofRoot . "/xhprof_lib/utils/xhprof_runs.php";
+
+$xhprofHost = 'xhprof.dev';
+$xhprofRuns = new XHProfRuns_Default();
+
+$namespace = "wtw";
+$runId = $xhprofRuns->save_run($xhprofData, $namespace);
+$profilingUrl = 'http://' . $xhprofHost. '/index.php?' .
+'run=' . $runId .
+'&source=' . $namespace;
+
+error_log(sprintf('-----------------[ %s ]----------------', $profilingUrl));
+
