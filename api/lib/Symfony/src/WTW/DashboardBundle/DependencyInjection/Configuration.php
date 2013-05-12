503e9688128821fa8d47caa9c41d3ac881e965d3
revue-de-presse <contact+github@revue-de-presse.org>
2013-05-12 20:15:34 +0200

    [cache warmup] Implements offline mode to prevent network-dependent dependencies from breaking cache clear

--- api/lib/Symfony/src/WTW/DashboardBundle/DependencyInjection/Configuration.php
+++ api/lib/Symfony/src/WTW/DashboardBundle/DependencyInjection/Configuration.php
@@ -20,9 +20,13 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('wtw_dashboard');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+                ->booleanNode('offline_mode')
+                    ->info('Prevents network-dependent installation of packages on cache warm-up')
+                    ->defaultFalse()
+                ->end()
+            ->end();
 
         return $treeBuilder;
     }
--- api/lib/Symfony/src/WTW/DashboardBundle/WTWDashboardBundle.php
+++ api/lib/Symfony/src/WTW/DashboardBundle/WTWDashboardBundle.php
@@ -2,8 +2,17 @@
 
 namespace WTW\DashboardBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder,
+    Symfony\Component\HttpKernel\Bundle\Bundle;
+use WTW\DashboardBundle\Compiler\AssetManagementCompilerPass;
 
 class WTWDashboardBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new AssetManagementCompilerPass());
+    }
 }
