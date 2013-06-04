7193c89a9a881a30b0a8191ac4ddcb939efa89ca
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2013-06-04 23:14:46 +0200

    [api][wordpress][isolation] Isolates Wordpress API bundle

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-if ( isset($class_application ) ) $class_application::display();
-else error_log('bootstrap script not loaded from "' . __FILE__ . '"');
--- routines/routine.import.language.files.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$class_i18n = $class_application::getI18nClass();
-
-$class_i18n::importI18nData();
