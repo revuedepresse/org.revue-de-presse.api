76d0eae2f4adfd1ee3188e5512f003bb6b74d0c1
revue-de-presse <contact+github@revue-de-presse.org>
2013-06-04 23:16:00 +0200

    [vendor] Updates to Symfony 2.3

--- api/lib/Symfony/app/AppKernel.php
+++ api/lib/Symfony/app/AppKernel.php
@@ -19,37 +19,15 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
-            new FOS\RestBundle\FOSRestBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
-            new FOS\TwitterBundle\FOSTwitterBundle(),
-            new Propel\PropelBundle\PropelBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Sp\BowerBundle\SpBowerBundle(),
-            new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
-            new WTW\API\FacebookBundle\WTWAPIFacebookBundle(),
-            new WTW\API\DataMiningBundle\WTWAPIDataMiningBundle(),
-            new WTW\API\GithubBundle\WTWAPIGithubBundle(),
-            new WTW\API\GoogleDriveBundle\WTWAPIGoogleDriveBundle(),
-            new WTW\API\TwitterBundle\WTWAPITwitterBundle(),
             new WTW\CodeGeneration\QualityAssuranceBundle\WTWCodeGenerationQualityAssuranceBundle(),
-            new WTW\CodeGeneration\AnalysisBundle\WTWCodeGenerationAnalysisBundle(),
-            new WTW\DashboardBundle\WTWDashboardBundle(),
-            new WTW\Documentation\MarkdownBundle\WTWDocumentationMarkdownBundle(),
-            new WTW\Legacy\ProviderBundle\WTWLegacyProviderBundle(),
-            new WTW\UserBundle\WTWUserBundle(),
             new WTW\API\WordpressBundle\WTWAPIWordpressBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test', 'prof'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
-
-        if (in_array($this->getEnvironment(), array('prof'))) {
-            $bundles[] = new Jns\Bundle\XhprofBundle\JnsXhprofBundle();
         }
 
         return $bundles;
