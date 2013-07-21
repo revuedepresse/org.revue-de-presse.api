aad29bb4f3114097be3937fa65f0a73125ac91aa
revue-de-presse <contact+github@revue-de-presse.org>
2013-07-21 20:33:43 +0200

    [weaving the web][user] Creates form type

--- src/WeavingTheWeb/Bundle/UserBundle/Form/Type/UserType.php
+++ src/WeavingTheWeb/Bundle/UserBundle/Form/Type/UserType.php
@@ -3,7 +3,8 @@
 namespace WeavingTheWeb\Bundle\UserBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType,
-    Symfony\Component\Form\FormBuilderInterface;
+    Symfony\Component\Form\FormBuilderInterface,
+    Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
  * Class UserType
@@ -11,12 +12,32 @@ use Symfony\Component\Form\AbstractType,
  */
 class UserType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $builder
+            ->add('twitter_username', 'text')
+            ->add('username', 'text')
+            ->add('email', 'text')
+            ->add('firstName', 'text', ['required' => false])
+            ->add('lastName', 'text', ['required' => false])
+            ->add('disabled', 'checkbox', ['mapped' => false]) // TODO As a weaver, I can prevent my data from being collected
+            ->add('save', 'submit');
     }
 
+    /**
+     * @return string
+     */
     public function getName()
     {
         return 'user';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(['data_class' => 'WTW\UserBundle\Entity\User']);
     }
 }
