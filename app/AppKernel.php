b7a240cc35d05fb483ff8bd3adffa78a153555bd
revue-de-presse <contact+github@revue-de-presse.org>
2013-07-27 20:29:15 +0200

    [vendor] Loads ElaoWebProfilerExtraBundle in development environment

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -40,6 +40,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Elao\WebProfilerExtraBundle\WebProfilerExtraBundle();
         }
 
         return $bundles;
