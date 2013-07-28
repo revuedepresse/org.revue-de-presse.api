e90e9df9a258ef9576796f58219edbe2a7979039
revue-de-presse <contact+github@revue-de-presse.org>
2013-07-28 10:23:58 +0200

    [security][twitter][connect] Repairs connect to twitter

--- src/WTW/DashboardBundle/Security/User/Provider/TwitterUserProvider.php
+++ src/WTW/DashboardBundle/Security/User/Provider/TwitterUserProvider.php
@@ -5,6 +5,7 @@ namespace WTW\DashboardBundle\Security\User\Provider;
 use FOS\UserBundle\Doctrine\UserManager;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException,
     Symfony\Component\Security\Core\Exception\UnsupportedUserException,
+    Symfony\Component\Security\Core\Exception\AuthenticationException,
     Symfony\Component\Security\Core\User\UserProviderInterface,
     Symfony\Component\Security\Core\User\UserInterface,
     Symfony\Component\HttpFoundation\Session\Session,
@@ -18,60 +19,134 @@ use \TwitterOAuth;
  */
 class TwitterUserProvider implements UserProviderInterface
 {
-   /**
-    * @var \Twitter
-    */
-   protected $twitter_oauth;
-   protected $userManager;
-   protected $validator;
-   protected $session;
+    /**
+     * @var \Twitter
+     */
+    protected $twitterOauth;
 
-   public function __construct(
-       \TwitterOAuth $twitter_oauth,
-       UserManager $userManager,
-       Validator $validator,
-       Session $session)
-   {
-       $this->twitter_oauth = $twitter_oauth;
-       $this->userManager = $userManager;
-       $this->validator = $validator;
-       $this->session = $session;
-   }
+    /**
+     * @var \FOS\UserBundle\Doctrine\UserManager
+     */
+    protected $userManager;
 
-   public function supportsClass($class)
-   {
+    /**
+     * @var \Symfony\Component\Validator\Validator
+     */
+    protected $validator;
+
+    /**
+     * @var \Symfony\Component\HttpFoundation\Session\Session
+     */
+    protected $session;
+
+    /**
+     * @param \FOS\UserBundle\Doctrine\UserManager $userManager
+     */
+    public function setUserManager($userManager)
+    {
+        $this->userManager = $userManager;
+    }
+
+    /**
+     * @return \FOS\UserBundle\Doctrine\UserManager
+     */
+    public function getUserManager()
+    {
+        return $this->userManager;
+    }
+
+    /**
+     * @param \Symfony\Component\Validator\Validator $validator
+     */
+    public function setValidator($validator)
+    {
+        $this->validator = $validator;
+    }
+
+    /**
+     * @return \Symfony\Component\Validator\Validator
+     */
+    public function getValidator()
+    {
+        return $this->validator;
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Session\Session $session
+     */
+    public function setSession($session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Session\Session
+     */
+    public function getSession()
+    {
+        return $this->session;
+    }
+
+    protected $twitterApiVersion;
+
+    /**
+     * @param \Twitter $twitterOauth
+     */
+    public function setTwitterOauth($twitterOauth)
+    {
+        $this->twitterOauth = $twitterOauth;
+    }
+
+    /**
+     * @return \Twitter
+     */
+    public function getTwitterOauth()
+    {
+        return $this->twitterOauth;
+    }
+
+    public function supportsClass($class)
+    {
        return $this->userManager->supportsClass($class);
-   }
+    }
 
-   public function findUserByTwitterUsername($twitterUsername)
-   {
+    public function findUserByTwitterUsername($twitterUsername)
+    {
        return $this->userManager->findUserBy(array('twitter_username' => $twitterUsername));
-   }
+    }
 
-   public function loadUserByUsername($username)
-   {
+    public function loadUserByUsername($username)
+    {
        $user = $this->findUserByTwitterUsername($username);
 
-       $this->twitter_oauth->setOAuthToken($this->session->get('access_token'), $this->session->get('access_token_secret'));
+       $this->twitterOauth->setOAuthToken($this->session->get('access_token'), $this->session->get('access_token_secret'));
 
        try {
-           $info = $this->twitter_oauth->get('account/verify_credentials');
-       } catch (Exception $e) {
+           $info = $this->twitterOauth->get('account/verify_credentials');
+       } catch (\Exception $e) {
            $info = null;
        }
 
        if (!empty($info)) {
+           if (isset($info->errors) && is_array($info->errors) && count($info->errors)) {
+               throw new AuthenticationException($info->errors[0]->message, $info->errors[0]->code);
+           }
+
            if (empty($user)) {
                $user = $this->userManager->createUser();
-               $user->setEnabled(true);
-               $user->setPassword('');
+               $user->setEnabled(false);
+               $user->setLocked(false);
            }
 
            $username = $info->screen_name;
 
            $user->setTwitterID($info->id);
            $user->setTwitterUsername($username);
-           $user->setEmail($username . '@twitter.com');
+
+           $email = $user->getEmail();
+           if (is_null($email) || (strlen(trim($email)) === 0)) {
+               $user->setEmail($username . '@twitter.com');
+           }
 
            if (null === $user->getUsername()) {
                $user->setUsername($username);
@@ -86,14 +161,14 @@ class TwitterUserProvider implements UserProviderInterface
        }
 
        return $user;
-   }
+    }
 
-   public function refreshUser(UserInterface $user)
-   {
+    public function refreshUser(UserInterface $user)
+    {
        if (!$this->supportsClass(get_class($user)) || !$user->getTwitterID()) {
            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
        }
 
        return $this->loadUserByUsername($user->getTwitterID());
-   }
+    }
 }
