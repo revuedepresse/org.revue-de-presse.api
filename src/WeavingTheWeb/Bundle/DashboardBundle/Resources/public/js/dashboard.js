7087cae95879b3cffab2941c62705d14d9470cb7
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2013-07-28 12:19:38 +0200

    [dashboard] Renames WTWDashboardBundle to WeavingTheWebDashboardBundle

---
+++ src/WeavingTheWeb/Bundle/DashboardBundle/Resources/public/js/dashboard.js
@@ -0,0 +1,45 @@
+
+$saveQueryButton = $('#action-save-query');
+$queries = $('div.query button');
+
+if ($saveQueryButton.length > 0) {
+    $saveQueryButton.click(function (event) {
+        var saveSqlUrl = Routing.generate('weaving_the_web_dashboard_save_sql'),
+            sql = $('#sql').val();
+
+        $.post(saveSqlUrl, {
+           sql: sql
+        }, function (data) {
+            var notification = $('#notification');
+
+            notification.text(data.result);
+            notification.parent().removeClass('alert-error');
+            notification.parent().removeClass('alert-block');
+            notification.parent().removeClass('alert-success');
+            notification.parent().addClass('alert alert-' + data.type);
+        });
+
+        event.stopPropagation();
+        event.preventDefault();
+
+        return false;
+    });
+}
+
+$queries.bind('click', function(event) {
+    var $button = $(event.currentTarget),
+        sql = $button.parent().find('.sql').text(),
+        trimmedSql = sql.toLowerCase().trim();
+
+    if (
+        (trimmedSql.indexOf('# show') == 0) ||
+        (trimmedSql.indexOf('# count') == 0) ||
+        (trimmedSql.indexOf('select') == 0) ||
+        (trimmedSql.indexOf('create table') == 0) ||
+        (trimmedSql.indexOf('drop table') == 0)) {
+        $('#sql').val(sql);
+        $('.container form').submit();
+    }
+
+    event.preventDefault();
+});
---
+++ src/WeavingTheWeb/Bundle/DashboardBundle/Validator/Constraints/Query.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace WeavingTheWeb\Bundle\DashboardBundle\Validator\Constraints;
+
+use Symfony\Component\Translation\Translator,
+    Symfony\Component\Validator\Constraint;
+
+/**
+ * Class Query
+ *
+ * @package WeavingTheWeb\Bundle\DashboardBundle\Validator\Contraints
+ * @author Thierry Marianne <thierry.marianne@weaving-the-web.org>
+ */
+class Query extends Constraint
+{
+    public $deleteDataViolation = 'constraint_violation.query.delete';
+
+    public $nonEmptyViolation= 'constraint_violation.query.non_empty';
+
+    public $truncateTableViolation= 'constraint_violation.query.truncate';
+
+    public $dropDataViolation= 'constraint_violation.query.drop';
+
+    public $alterSchemaViolation= 'constraint_violation.query.alter';
+
+    public $grantPrivilegeViolation= 'constraint_violation.query.grant';
+
+    public function validatedBy()
+    {
+        return 'weaving_the_web.validator.query';
+    }
+}
