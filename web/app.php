848025e7bdfb0c7b73459e944a804fdc0e63dbb0
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2013-07-30 20:01:10 +0200

    [user cache] Starts using Apc in production

--- web/app.php
+++ web/app.php
@@ -5,20 +5,13 @@ use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
-/*
-$loader = new ApcClassLoader('sf2', $loader);
+$loader = new ApcClassLoader('dashboard', $loader);
 $loader->register(true);
-*/
 
 require_once __DIR__ . '/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
 
 Request::setTrustedProxies(['127.0.0.1']);
 $request  = Request::createFromGlobals();
