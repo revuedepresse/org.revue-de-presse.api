510404ef3bb87584efe0d8b104a85263082347d0
revue-de-presse <contact+github@revue-de-presse.org>
2013-08-02 03:14:39 +0200

    [ci] Setup environment dedicated to behaviour driven development

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,7 +36,7 @@ class AppKernel extends Kernel
             new WeavingTheWeb\Bundle\UserBundle\WeavingTheWebUserBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test', 'bdd'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
