a3ce6e00ac98f5a63faf9cd29a44a2092c4014a9
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2013-08-02 03:14:39 +0200

    [ci] Setup environment dedicated to behaviour driven development

--- web/app.php
+++ web/app.php
@@ -1,7 +1,7 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\ClassLoader\ApcClassLoader,
+    Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
 
