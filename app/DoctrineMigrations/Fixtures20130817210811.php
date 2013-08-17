3fac0ae7b1ad42de57833797d86d62ebdc57dae6
revue-de-presse <contact+github@revue-de-presse.org>
2013-08-17 22:49:37 +0200

    [fixtures] Imports additional fixtures to load administration user

---
+++ app/DoctrineMigrations/Fixtures20130817210811.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20130817210811 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+
+        $fixtures = file_get_contents(__DIR__ . '/fixtures/fixtures_legacy.sql');
+        $this->addSql($fixtures);
+
+        $fixtures = file_get_contents(__DIR__ . '/fixtures/fixtures_default_perspectives.sql');
+        $this->addSql($fixtures);
+
+        $fixtures = file_get_contents(__DIR__ . '/fixtures/fixtures_administration.sql');
+        $this->addSql($fixtures);
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+    }
+}
---
+++ app/DoctrineMigrations/fixtures/fixtures_administration.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO `weaving_user` (`usr_id`, `usr_twitter_id`, `usr_twitter_username`, `grp_id`, `usr_avatar`, `usr_first_name`, `usr_full_name`, `usr_last_name`, `usr_middle_name`, `usr_phone`, `usr_status`, `usr_user_name`, `usr_username_canonical`, `usr_email`, `usr_email_canonical`, `usr_password`, `usr_password_requested_at`, `usr_salt`, `usr_locked`, `usr_credentials_expired`, `usr_credentials_expires_at`, `usr_confirmation_token`, `usr_expired`, `usr_expires_at`, `usr_last_login`) VALUES
+(1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, 'Gordon', 'gordon', 'thyssy@gmail.com', 'thyssy@gmail.com', 'd6ltcVgQp2fB4oHWx2p6VPjTJKstw16PrqWPWeVJF4A2S6qZRxI4xnWp0M5aOqf00qkc07iU/mEC9JvJfN9T6Q==', NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL);
+
+
+--
+-- Constraints for table `weaving_user_role`
+--
+ALTER TABLE `weaving_user_role`
+  ADD CONSTRAINT `FK_95AD963FD60322AC` FOREIGN KEY (`role_id`) REFERENCES `weaving_role` (`id`),
+  ADD CONSTRAINT `FK_95AD963FA76ED395` FOREIGN KEY (`user_id`) REFERENCES `weaving_user` (`usr_id`);
+
+INSERT INTO `weaving_role` (`id`, `name`, `role`) VALUES
+(1, 'Weaver', 'ROLE_USER'),
+(2, 'Super Weaver', 'ROLE_ADMIN'),
+(3, 'Über Weaver', 'ROLE_SUPER_ADMIN');
