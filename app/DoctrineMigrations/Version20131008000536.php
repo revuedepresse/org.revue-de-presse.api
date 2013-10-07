ca3c0c75f4f59e2ca3739226b2f9dc37ec2f9a68
revue-de-presse <contact+github@revue-de-presse.org>
2013-10-08 00:06:13 +0200

    [performance][twitter] Adds index to screen name and status id attributes of user stream

---
+++ app/DoctrineMigrations/Version20131008000536.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20131008000536 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("CREATE INDEX screen_name ON weaving_twitter_user_stream (ust_full_name)");
+        $this->addSql("CREATE INDEX status_id ON weaving_twitter_user_stream (ust_status_id)");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("DROP INDEX screen_name ON weaving_twitter_user_stream");
+        $this->addSql("DROP INDEX status_id ON weaving_twitter_user_stream");
+    }
+}
