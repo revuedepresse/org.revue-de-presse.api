b494a3fd9e61cb74ca70211944600545d708f836
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2013-11-03 21:36:26 +0100

    [twitter] Implements application authentication

---
+++ src/WeavingTheWeb/Bundle/ApiBundle/Entity/TokenType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace WeavingTheWeb\Bundle\ApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Token type
+ *
+ * @ORM\Table(name="weaving_token_type")
+ * @ORM\Entity()
+ */
+class TokenType
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    protected $name;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return TokenType
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
