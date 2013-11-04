a0bda4b59e0175dd2ff03352a7e56b77454cc509
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2013-11-04 21:40:37 +0100

    [twitter] Removes duplicated creation of unique index on twitter id column

--- app/DoctrineMigrations/Version20131022234525.php
+++ app/DoctrineMigrations/Version20131022234525.php
@@ -15,7 +15,6 @@ class Version20131022234525 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
-        $this->addSql("CREATE UNIQUE INDEX unique_twitter_id ON weaving_user (usr_twitter_id)");
         $this->addSql("ALTER TABLE weaving_access_token CHANGE updated_at updated_at DATETIME DEFAULT NULL");
     }
 
@@ -25,6 +24,5 @@ class Version20131022234525 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
         $this->addSql("ALTER TABLE weaving_access_token CHANGE updated_at updated_at DATETIME NOT NULL");
-        $this->addSql("DROP INDEX unique_twitter_id ON weaving_user");
     }
 }
