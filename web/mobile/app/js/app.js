6781a434bf89968080844957a2f472c84ff6f90a
revue-de-presse <contact+github@revue-de-presse.org>
2014-01-25 18:45:20 +0100

    [mobile] Imports mobile application

---
+++ web/mobile/app/js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var weaverApp = angular.module('weaverApp', [
+    'ngRoute',
+    'twitterControllers'
+]);
+
+weaverApp.config(
+    ['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: '/mobile/app/partials/tweets.html',
+            controller: 'ShowTweetsAction'
+        });
+        $routeProvider.otherwise({redirectTo: '/'});
+    }]);
