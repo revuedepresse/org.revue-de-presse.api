009739ca53785466aa60b8cadd1972ee86360b69
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2014-02-16 23:51:32 +0100

    [mobile] Tested cache of starring actions

--- web/mobile/app/js/services/twitter.js
+++ web/mobile/app/js/services/twitter.js
@@ -25,23 +25,3 @@ weaverApp.factory('twitter', ['$http', '$location', '$log',
         };
     }]
 );
-
-weaverApp.factory('offlineCache', ['$angularCacheFactory',
-    function (cacheFactory) {
-        return {
-            getLocalStorageCache: function () {
-                var cache = cacheFactory.get('localStorageCache');
-                if (cache === undefined) {
-                    cache = cacheFactory('localStorageCache', {
-                        maxAge: 7 * 24 * 3600, // Items added to this cache expire after 1 week.
-                        cacheFlushInterval: 24 * 3600, // This cache will clear itself every day.
-                        deleteOnExpire: 'aggressive', // Items will be deleted from this cache right when they expire.
-                        storageMode: 'localStorage' // This cache will sync itself with `localStorage`.
-                    });
-                }
-
-                return cache;
-            }
-        };
-    }]
-);
