58eb107680d79103d52a0a66a7c60e8da371da90
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2014-05-27 00:35:26 +0200

    [mobile] Implements infinite scroll

--- web/mobile/app/js/services/twitter.js
+++ web/mobile/app/js/services/twitter.js
@@ -4,23 +4,40 @@ weaverApp.factory('twitter', ['$http', '$location', '$log',
     function ($http, $location, $log) {
         return {
             showMoreTweets: function ($scope, $routeParams) {
-                var host = $location.protocol() + '://' + $location.host();
-                var showLatestTweetsUrl = host + '/twitter/tweet/latest?username=' + $routeParams.username;
+                var host = $location.protocol() + '://' + $location.host(),
+                    showLatestTweetsUrl = host + '/twitter/tweet/latest?username=' + $routeParams.username,
+                    hash = '_' + $routeParams.username
 
-                $http.get(showLatestTweetsUrl).success(function (tweets) {
-                    if (undefined === $scope.tweets) {
-                        $scope.tweets = tweets
-                    } else {
-                        var k;
-                        for (k = 0; k < 50; k++) {
-                            $scope.tweets.push(tweets[k])
+                if ($routeParams.lastStatusId !== undefined) {
+                    showLatestTweetsUrl = showLatestTweetsUrl + '&lastStatusId=' + $routeParams.lastStatusId;
+                    hash = hash + '_' + $routeParams.lastStatusId;
+                }
+
+                if ($scope.lockedRequests === undefined) {
+                    $scope.lockedRequests = {};
+                }
+
+
+                if ($scope.lockedRequests[hash] === undefined) {
+                    $scope.lockedRequests[hash] = true;
+
+                    $http.get(showLatestTweetsUrl).success(function (tweets) {
+                        if (undefined === $scope.tweets) {
+                            $scope.tweets = tweets
+                        } else {
+                            var k;
+                            for (k = 0; k < 50; k++) {
+                                $scope.tweets.push(tweets[k])
+                            }
                         }
-                    }
-                }).error(function (data) {
-                    if ($log !== undefined) {
-                        $log.error(data)
-                    }
-                });
+
+                        $scope.lockedRequests[hash] = undefined;
+                    }).error(function (data) {
+                        if ($log !== undefined) {
+                            $log.error(data)
+                        }
+                    });
+                }
            }
         };
     }]
