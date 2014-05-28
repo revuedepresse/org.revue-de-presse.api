f6a2b75d7d9bf8a2fbf9473daecd0bfd22be859f
revue-de-presse <contact+github@revue-de-presse.org>
2014-05-29 00:05:11 +0200

    [mobile] Collapses twitter statuses using semantic accordion

--- web/mobile/app/js/services/twitter.js
+++ web/mobile/app/js/services/twitter.js
@@ -2,8 +2,16 @@
 
 weaverApp.factory('twitter', ['$http', '$location', '$log', '$routeParams',
     function ($http, $location, $log, $routeParams) {
+        var initializeStatuses = function ($scope) {
+            if ($scope.tweets === undefined) {
+                $scope.tweets = [];
+            }
+        }
+
         var showStatuses = function ($scope, tweets) {
-            if (undefined === $scope.tweets) {
+            initializeStatuses($scope);
+
+            if ($scope.tweets.length === 0) {
                 $scope.tweets = tweets
             } else {
                 var k;
@@ -16,6 +24,15 @@ weaverApp.factory('twitter', ['$http', '$location', '$log', '$routeParams',
         return {
             showStatuses: showStatuses,
             showMoreTweets: function ($scope, $routeParams) {
+                var lastStatus;
+
+                initializeStatuses($scope);
+
+                if ($scope.tweets.length !== 0) {
+                    lastStatus = $scope.tweets[$scope.tweets.length - 1];
+                    $routeParams.lastStatusId = lastStatus.id;
+                }
+
                 var host = $location.protocol() + '://' + $location.host(),
                     showLatestTweetsUrl = host + '/twitter/tweet/latest?username=' + $routeParams.username,
                     hash = '_' + $routeParams.username;
@@ -29,11 +46,14 @@ weaverApp.factory('twitter', ['$http', '$location', '$log', '$routeParams',
                     $scope.lockedRequests = {};
                 }
 
-
                 if ($scope.lockedRequests[hash] === undefined) {
                     $scope.lockedRequests[hash] = true;
 
                     $http.get(showLatestTweetsUrl).success(function (tweets) {
+                        _.each(tweets, function () {
+
+                        });
+
                         showStatuses($scope, tweets);
 
                         $scope.lockedRequests[hash] = undefined;
