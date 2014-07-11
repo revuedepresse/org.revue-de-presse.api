ea48fd547353db55ebdc77242499004135b6f202
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2014-07-11 23:51:21 +0200

    [migration] Renames header and message table

--- app/DoctrineMigrations/Version20140630221406.php
+++ app/DoctrineMigrations/Version20140630221406.php
@@ -18,6 +18,8 @@ class Version20140630221406 extends AbstractMigration
         $this->addSql("CREATE TABLE message_property (property_id INT NOT NULL, message_id INT NOT NULL, type INT NOT NULL, INDEX IDX_82D4D8CF537A1329 (message_id), INDEX IDX_82D4D8CF549213EC (property_id), PRIMARY KEY(property_id, message_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB");
         $this->addSql("ALTER TABLE message_property ADD CONSTRAINT FK_82D4D8CF537A1329 FOREIGN KEY (message_id) REFERENCES message (msg_id)");
         $this->addSql("ALTER TABLE message_property ADD CONSTRAINT FK_82D4D8CF549213EC FOREIGN KEY (property_id) REFERENCES property (id)");
+        $this->addSql("RENAME TABLE weaving_header to header");
+        $this->addSql("RENAME TABLE weaving_message to message");
         $this->addSql("ALTER TABLE header ADD labelled TINYINT(1) DEFAULT '0' NOT NULL, ADD gmail_message_id VARCHAR(255) DEFAULT NULL, ADD gmail_thread_id VARCHAR(255) DEFAULT NULL");
         $this->addSql("SET foreign_key_checks = 0;");
         $this->addSql("ALTER TABLE message CHANGE hdr_id hdr_id INT NOT NULL");
