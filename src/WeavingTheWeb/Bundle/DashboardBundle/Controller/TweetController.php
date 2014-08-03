35b362dd681d5a7aad4c629ecdfda83232089838
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2014-08-03 20:11:23 +0200

    [ui] Upgrades application to flat responsive design

--- src/WeavingTheWeb/Bundle/DashboardBundle/Controller/TweetController.php
+++ src/WeavingTheWeb/Bundle/DashboardBundle/Controller/TweetController.php
@@ -26,10 +26,15 @@ class TweetController extends Controller
     {
         $translator = $this->get('translator');
 
+        $searchIndex = $this->container->getParameter('twitter_search_index');
+        $finder = $this->container->get('fos_elastica.finder.' . $searchIndex . '.user_status');
+        $gitHubRelatedTweets = $finder->find('github');
+
         return $this->render(
-            'WeavingTheWebDashboardBundle:Tweet:showGitHubTweets.html.twig', array(
+            'WeavingTheWebDashboardBundle:Tweet:showGitHubTweets.html.twig', [
                 'active_menu_item' => 'tweets',
+                'tweets'           => $gitHubRelatedTweets,
                 'title'            => $translator->trans('title.tweet.github', [], 'dashboard')
-            ));
+            ]);
     }
 } 
