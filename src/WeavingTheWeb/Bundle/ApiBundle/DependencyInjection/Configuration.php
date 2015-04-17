dab16ea3b5f12d83438edb6d26c13b4a5efce7d4
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2015-04-17 23:23:02 +0200

    Mutualizes search provider classes

--- src/WeavingTheWeb/Bundle/ApiBundle/DependencyInjection/Configuration.php
+++ src/WeavingTheWeb/Bundle/ApiBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('weaving_the_web_api');
+        $treeBuilder->root('weaving_the_web_api');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
