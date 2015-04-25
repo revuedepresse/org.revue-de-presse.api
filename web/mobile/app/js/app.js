1b3be17ab29e7bd6e14b1078ef992ed05a0dd16d
revue-de-presse <contact+github@revue-de-presse.org>
2015-04-25 23:07:26 +0200

    Uses access token to consume Twitter API

--- web/mobile/app/js/app.js
+++ web/mobile/app/js/app.js
@@ -5,26 +5,12 @@ var weaverApp = angular.module('weaverApp', [
     'infinite-scroll',
     'jmdobry.angular-cache',
     'strictHttpsFilter',
-    'satellizer',
-    'authorizeApplication',
     'twitterStatuses',
     'twitterBookmarks'
 ]);
 
-weaverApp.config(['$authProvider', function($authProvider) {
-    var clientId = '## FILL ME ##';
-
-    $authProvider.oauth2({
-      name: 'Weaving The Web',
-      url: '/oauth/v2/auth',
-      redirectUri: '/mobile/app.html',
-      clientId: clientId,
-      authorizationEndpoint: 'http://dashboard.dev/oauth/v2/auth'
-    });
-}]);
-
 weaverApp.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/:username', {
+    $routeProvider.when('/:username/:oauth_token', {
         controller: 'ShowStatusesAction',
         templateUrl: '/mobile/app/partials/statuses.html'
     });
@@ -32,11 +18,6 @@ weaverApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/bookmarks/:username', {
         controller: 'ShowBookmarksAction',
         templateUrl: '/mobile/app/partials/bookmarks.html'
-    });
-
-    $routeProvider.when('/', {
-        controller: 'AuthorizeApplicationAction',
-        templateUrl: '/mobile/app/partials/sign-in.html'
     });
 
     $routeProvider.otherwise({redirectTo: '/'});
