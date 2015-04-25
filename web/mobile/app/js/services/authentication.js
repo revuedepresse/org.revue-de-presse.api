8b3696ad09529d3cf2e1d48116ea0be50fbd41c6
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2015-04-25 22:45:06 +0200

    Starts using satellizer for OAuth flow

--- web/mobile/app/js/services/authentication.js
+++ web/mobile/app/js/services/authentication.js
@@ -1,22 +1,32 @@
 'use strict';
 
-weaverApp.factory('authentication', ['$q',
-    function ($q) {
+weaverApp.factory('oauthAuthentication', ['$window', '$q', '$location',
+    function ($window, $q, $location) {
+
+        var authorizeClient = function () {
+            var clientId = '## FILL ME ##';
+            var redirectUri = $location.protocol() + '://' + $location.host() + '/mobile/app.html';
+
+            $window.location = '/oauth/v2/auth?client_id=' + clientId +
+                '&response_type=code' +
+                '&redirect_uri=' + encodeURIComponent(redirectUri)
+            ;
+        };
+
         return {
-            'response': function (response) {
-                if (!_.isUndefined(response.data) && !_.isUndefined(response.data.csrf_token)) {
-                    return $q.reject({
-                        data: {
-                            info: 'Please authenticate first in order to use this application.'
-                        },
-                        status: 302,
-                        headers: response.headers,
-                        config: response.config
-                    });
+            request: function(config) {
+                config.headers = config.headers || {};
+                if ($window.oauthToken) {
+                    config.headers.Authorization = 'Bearer ' + $window.oauthToken;
+                } else {
+                    authorizeClient();
                 }
 
-                return response;
+                return config || $q.when(config);
+            },
+            response: function(response) {
+                return response || $q.when(response);
             }
-        }
-    }
-]);
+        };
+    }]
+);
