3f5677227de494878f43ed55a3cae73caa9f72db
revue-de-presse <contact+github@revue-de-presse.org>
2015-04-27 23:14:18 +0200

    Declares options route

--- src/WeavingTheWeb/Bundle/ApiBundle/Controller/TwitterController.php
+++ src/WeavingTheWeb/Bundle/ApiBundle/Controller/TwitterController.php
@@ -7,11 +7,27 @@ use FOS\RestBundle\Controller\Annotations as Rest,
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 
+use Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\HttpFoundation\Response;
+
 /**
  * @Rest\NamePrefix("weaving_the_web_api_")
  */
 class TwitterController extends ResourceController
 {
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function optionsUsersStreamsAction(Request $request)
+    {
+        return new Response('', 200, [
+            'Access-Control-Allow-Origin' => $request->getHost(),
+            'Access-Control-Allow-Methods' => 'GET, POST',
+            'Access-Control-Allow-Headers' => 'Autorization'
+        ]);
+    }
+
     /**
      * Get stream items
      *
