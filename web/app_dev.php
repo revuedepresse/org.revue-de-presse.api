4492da8a6c3b59ce8cd60d081de0d015f97603ef
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2015-06-03 21:47:12 +0200

    Adds docker provisioning

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,25 +3,6 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-//umask(0000);
-
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-if (
-    (
-        isset($_SERVER['HTTP_CLIENT_IP']) ||
-        !in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '## FILL WITH IP ADDRESS ##', 'fe80::1', '::1'])
-    ) && (
-        ! isset($_SERVER['HTTP_X_FORWARDED_FOR']) ||
-        ($_SERVER['HTTP_X_FORWARDED_FOR'] !== '## FILL IP ADDRESS ##')
-    )
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-}
-
 require_once __DIR__.'/../app/autoload.php';
 Debug::enable();
 
