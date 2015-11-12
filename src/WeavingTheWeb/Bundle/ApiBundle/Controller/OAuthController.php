ad6ce0201948b529b1aa4aabaf0649aa0ad6b591
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2015-11-12 15:39:56 +0100

    Tests OAuth access token generation step by step

--- src/WeavingTheWeb/Bundle/ApiBundle/Controller/OAuthController.php
+++ src/WeavingTheWeb/Bundle/ApiBundle/Controller/OAuthController.php
@@ -24,14 +24,29 @@ class OAuthController extends Controller
      */
     public function callbackAction(Request $request)
     {
-        $redirectUri = $request->getSchemeAndHttpHost() . $this->generateUrl('weaving_the_web_api_oauth_callback');
+        /** @var \WeavingTheWeb\Bundle\DashboardBundle\Repository\OAuth\ClientRepository $clientRepository */
+        $clientRepository = $this->container->get('weaving_the_web_dashboard.repository.oauth.client');
+
+        /** @var \WeavingTheWeb\Bundle\DashboardBundle\Entity\OAuth\Client $client */
+        $client = $clientRepository->findOneBy(['selected' => true]);
+        $logger = $this->get('logger');
+
+        if (is_null($client)) {
+            $clientId = $this->container->getParameter('api_client_id');
+            $clientSecret = $this->container->getParameter('api_client_secret');
+            $logger->info(sprintf('Fallback on application-wise OAuth client (client id: "%s")', $clientId));
+        } else {
+            $clientId = $client->getClientId();
+            $clientSecret = $client->getClientSecret();
+        }
 
         /** @var \Symfony\Component\Routing\Router $router */
         $router = $this->get('router');
+        $redirectUri = $request->getSchemeAndHttpHost() . $this->generateUrl('weaving_the_web_api_oauth_callback');
         $tokenUrl = $router->generate(
             'fos_oauth_server_token', [
-                'client_id' => $this->container->getParameter('api_client_id'),
-                'client_secret' => $this->container->getParameter('api_client_secret'),
+                'client_id' => $clientId,
+                'client_secret' => $clientSecret,
                 'code' => $request->query->get('code'),
                 'grant_type' => 'authorization_code',
                 'redirect_uri' => $redirectUri,
