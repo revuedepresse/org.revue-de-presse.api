374b27419ab82a4438723455e203dc2d95ae6fd7
revue-de-presse <contact+github@revue-de-presse.org>
2015-11-29 14:28:40 +0100

    Adds Vagrant support for development

---
+++ provisioning/scripts/ensure-required-files-exist.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function ensure_dir_exists {
+    export SCRIPT=/tmp/ensure-directory-exists
+    export FOLDER_PATH=$1
+
+    cat > $SCRIPT <<SCRIPT
+    echo 'Checking if "'$FOLDER_PATH'" directory exists.'
+    if [ ! -d $FOLDER_PATH ]
+    then
+        echo 'Creating missing directory.'
+        mkdir -p $FOLDER_PATH
+    else
+        echo 'Confirmed "'$FOLDER_PATH'" directory existed already.'
+    fi
+SCRIPT
+
+    echo "Executing the following script:"
+    echo "## --- START --- ##"
+    cat $SCRIPT
+    echo "## --- END ---##"
+
+    sudo /bin/bash -c "source $SCRIPT"
+}
+
+export COMPOSER_FOLDER=/home/vagrant/.composer
+export DEPLOY_FOLDER=/var/deploy/devobs
+export DOCUMENT_ROOT=/var/www/devobs
+
+ensure_dir_exists $COMPOSER_FOLDER
+ensure_dir_exists $DEPLOY_FOLDER
+
+echo "Checking if symbolic link exists."
+if [ ! -e $DOCUMENT_ROOT ]
+then
+    echo "Creating missing symbolic link."
+    ln -s $DEPLOY_FOLDER/current $DOCUMENT_ROOT
+else
+    echo "Confirmed symbolic link existed already."
+fi
+
+chown -R vagrant $COMPOSER_FOLDER
+chown -R vagrant $DEPLOY_FOLDER
