63336c3c34b0f7359c685cf288dfdfcc22435b9b
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2015-11-30 10:38:20 +0100

    Tries to run tests in container-based environment

--- tests/.run-test.sh
+++ tests/.run-test.sh
@@ -2,6 +2,8 @@
 
 (cd tests && bower install)
 
-for test in `cat package.json | grep 'test-' | grep -v 'test"' | cut -f '1' -d ':' | cut -d '"' -f 2`;\
-do /bin/bash -c 'npm run '$test || (break && exit 1); \
-done
+(for test in `cat package.json | grep 'test-' | grep -v 'test"' | cut -f '1' -d ':' | cut -d '"' -f 2`;\
+do /bin/bash -c 'npm run '$test || exit 1; \
+done)
+
+exit $?
