b40c4c8aa1a693a1732d7856b0ae93830afed68c
revue-de-presse <contact+github@revue-de-presse.org>
2015-11-30 11:57:58 +0100

    Install bower components exist before testing

--- tests/.run-test.sh
+++ tests/.run-test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-(cd tests && bower install)
+# Run all test suites successively
+
+source tests/.install-bower-components.sh
 
 (for test in `cat package.json | grep 'test-' | grep -v 'test"' | cut -f '1' -d ':' | cut -d '"' -f 2`;\
 do /bin/bash -c 'npm run '$test || exit 1; \
