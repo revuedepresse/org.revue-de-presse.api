28c0c3e6129db35dd6c2579ab03310cd844306ab
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2018-08-25 12:41:00 +0200

    Mark statuses as not found by inserting additional records for each of them

--- src/App/Status/Entity/NotFoundStatus.php
+++ src/App/Status/Entity/NotFoundStatus.php
@@ -4,6 +4,7 @@ namespace App\Status\Entity;
 
 use WeavingTheWeb\Bundle\ApiBundle\Entity\ArchivedStatus;
 use WeavingTheWeb\Bundle\ApiBundle\Entity\Status;
+use WeavingTheWeb\Bundle\ApiBundle\Entity\StatusInterface;
 
 class NotFoundStatus
 {
@@ -21,4 +22,34 @@ class NotFoundStatus
      * @var ArchivedStatus
      */
     private $archivedStatus = null;
+
+    /**
+     * @param $status
+     * @param $archivedStatus
+     */
+    public function __construct(Status $status = null, ArchivedStatus $archivedStatus = null)
+    {
+        $this->archivedStatus = $archivedStatus;
+        $this->status = $status;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return StatusInterface
+     */
+    public function getStatus()
+    {
+        if (is_null($this->status)) {
+            return $this->archivedStatus;
+        }
+
+        return $this->status;
+    }
 }
