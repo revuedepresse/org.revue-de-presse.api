1a896934c5ce41bf58f51dcec4e1dcc365abe115
revue-de-presse <contact+github@revue-de-presse.org>
2018-08-28 23:33:57 +0200

    Optimize with deflate module for Apache (#16)
    Remove sub-query and use datetime index properly
    Enable http2 and deflate modules for Apache
    Apply coalesce to nullable column

--- provisioning/containers/apache/tasks/setup-virtual-host.sh
+++ provisioning/containers/apache/tasks/setup-virtual-host.sh
@@ -13,7 +13,13 @@ a2enmod rewrite
 # Enable headers module for CORS
 a2enmod headers
 
+# Enable HTTP2
+a2enmod http2
+
 cd /etc/apache2
 ln -s `pwd`/sites-available/press-review.conf `pwd`/sites-enabled
+
+rm `pwd`/mods-enabled/deflate.conf
+ln -s /templates/mods-enabled/deflate.conf `pwd`/mods-enabled
 
 apache2-foreground &
