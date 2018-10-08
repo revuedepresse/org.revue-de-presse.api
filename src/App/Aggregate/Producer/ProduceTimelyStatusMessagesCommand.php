ddacfd09bafd9f9dd93781433bd997d2d88844c6
revue-de-presse <contact+github@revue-de-presse.org>
2018-10-08 21:15:48 +0200

    Sort timely status messages before publication (#43)

--- src/App/Aggregate/Producer/ProduceTimelyStatusMessagesCommand.php
+++ src/App/Aggregate/Producer/ProduceTimelyStatusMessagesCommand.php
@@ -77,18 +77,42 @@ class ProduceTimelyStatusMessagesCommand extends AggregateAwareCommand
             a.id aggregate_id,
             a.name aggregate_name,
             s.ust_created_at publication_date_time,
-            s.ust_full_name member_name
+            s.ust_full_name member_name,
+            s.ust_api_document json
             FROM weaving_aggregate a,
             weaving_status_aggregate sa,
             weaving_status s 
             WHERE sa.aggregate_id = a.id
             AND s.ust_id = sa.status_id
-            AND ust_created_at > DATE_SUB(now(), INTERVAL 1 WEEK)
+            AND ust_created_at > DATE_SUB(now(), INTERVAL 1 WEEK )
 QUERY
 ;
 
         $statement = $this->entityManager->getConnection()->executeQuery($query);
         $records = $statement->fetchAll();
+
+        $records = array_map(
+            function (array $record) {
+                $document = json_decode($record['json'], true);
+                $record['date_time'] = new \DateTime($document['created_at']);
+                $record['created_at'] = $document['created_at'];
+
+                return $record;
+            },
+            $records
+        );
+
+        usort($records, function ($leftRecord, $rightRecord) {
+            if ($leftRecord['date_time'] === $rightRecord['date_time']) {
+                return 0;
+            }
+
+            if ($leftRecord['date_time'] < $rightRecord['date_time']) {
+                return 1;
+            }
+
+            return -1;
+        });
 
         array_walk(
             $records,
@@ -99,6 +123,7 @@ QUERY
                     'aggregate_name' => $record['aggregate_name'],
                     'publication_date_time' => $record['publication_date_time'],
                     'member_name' => $record['member_name'],
+                    'created_at' => $record['created_at'],
                 ];
                 $this->producer->publish(serialize(json_encode($messageBody)));
             }
