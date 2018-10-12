5c1917f1da887348feb3efbe03f70f56f717a4a8
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2018-10-12 19:19:15 +0200

    Remove Twig Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,6 @@ class AppKernel extends Kernel
             // Standard edition framework
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
