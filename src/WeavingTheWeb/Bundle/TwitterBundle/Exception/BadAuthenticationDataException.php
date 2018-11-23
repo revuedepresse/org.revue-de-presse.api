1c8b73a4ab276c8bad34d24fa7f69fa45a875162
revue-de-presse <contact+github@revue-de-presse.org>
2018-11-23 19:32:58 +0100

    Handle bad authentication data error (expired tokens) (#117)

---
+++ src/WeavingTheWeb/Bundle/TwitterBundle/Exception/BadAuthenticationDataException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace WeavingTheWeb\Bundle\TwitterBundle\Exception;
+
+/**
+ * @author Thierry Marianne <thierry.marianne@weaving-the-web.org>
+ */
+class BadAuthenticationDataException extends \Exception
+{
+}
