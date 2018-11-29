0dc8e2ec570fa37c8c1b467b9e2fae08a1f88ad0
revue-de-presse <contact+github@revue-de-presse.org>
2018-11-29 23:31:39 +0100

    As an authenticated member, I can access a grid of aggregates (#131)

---
+++ src/App/Member/Controller/LoggedInMemberController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Member\Controller;
+
+use App\Member\MemberInterface;
+use App\Member\Repository\AuthenticationTokenRepository;
+use App\Security\Cors\CorsHeadersAwareTrait;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class LoggedInMemberController
+{
+    use CorsHeadersAwareTrait;
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    public $logger;
+
+    /**
+     * @var AuthenticationTokenRepository
+     */
+    public $authenticationTokenRepository;
+
+    /**
+     * @var string
+     */
+    public $environment;
+
+    /**
+     * @var string
+     */
+    public $allowedOrigin;
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getProfile(Request $request)
+    {
+        if ($request->isMethod('OPTIONS')) {
+            return $this->getCorsOptionsResponse(
+                $this->environment,
+                $this->allowedOrigin
+            );
+        }
+
+        $tokenId = $request->request->get(
+            'tokenId',
+            $request->headers->get('x-auth-admin-token'),
+            null
+        );
+
+        $corsHeaders = $this->getAccessControlOriginHeaders(
+            $this->environment,
+            $this->allowedOrigin
+        );
+
+        if (is_null($tokenId)) {
+            return $this->makeInvalidIdTokenResponse($corsHeaders);
+        }
+
+        $member = $this->authenticationTokenRepository->findMemberByTokenId($tokenId);
+        if (!($member instanceof MemberInterface)) {
+            return $this->makeInvalidIdTokenResponse($corsHeaders);
+        }
+
+        return new JsonResponse(
+            ['username' => $member->getTwitterUsername()],
+            200,
+            $corsHeaders
+        );
+    }
+
+    /**
+     * @param $corsHeaders
+     * @return JsonResponse
+     */
+    private function makeInvalidIdTokenResponse($corsHeaders): JsonResponse
+    {
+        return new JsonResponse(
+            'Invalid id token',
+            403,
+            $corsHeaders
+        );
+    }
+}
--- src/App/Member/Repository/AuthenticationTokenRepository.php
+++ src/App/Member/Repository/AuthenticationTokenRepository.php
@@ -2,8 +2,39 @@
 
 namespace App\Member\Repository;
 
+use App\Member\Authentication\Authenticator;
+use App\Member\Entity\AuthenticationToken;
+use App\Member\MemberInterface;
 use Doctrine\ORM\EntityRepository;
+use WTW\UserBundle\Repository\UserRepository;
 
 class AuthenticationTokenRepository extends EntityRepository
 {
+    /**
+     * @var UserRepository
+     */
+    public $memberRepository;
+
+    /**
+     * @var Authenticator
+     */
+    public $authenticator;
+
+    /**
+     * @param string $tokenId
+     * @return MemberInterface
+     */
+    public function findMemberByTokenId(string $tokenId): MemberInterface
+    {
+        try {
+            $tokenInfo = $this->authenticator->authenticate($tokenId);
+        } catch (\Exception $exception) {
+            return null;
+        }
+
+        /** @var AuthenticationToken $token */
+        $token = $this->findOneBy(['token' => $tokenInfo['sub']]);
+
+        return $token->getMember();
+    }
 }
