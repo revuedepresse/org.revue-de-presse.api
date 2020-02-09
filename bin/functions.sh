19a67762895de87ecfe1c1bef6df27844acef615
Thierry Marianne <thierry@marianne.io>
2020-02-09 17:11:35 +0100

    Fix retrieval of rabbitmq virtual host from local configuration file

--- bin/functions.sh
+++ bin/functions.sh
@@ -599,7 +599,7 @@ function configure_rabbitmq_user_privileges() {
 }
 
 function list_amqp_queues() {
-    local rabbitmq_vhost="$(cat <(cat app/config/parameters.yml | grep -v '#' | grep 'rabbitmq_vhost:' | cut -f 2 -d ':' | sed -e 's/[[:space:]]//g'))"
+    local rabbitmq_vhost="$(cat <(cat .env.local | grep amqp | sed -E 's#.+(/.+)$#\1#'))"
     docker exec -ti rabbitmq watch -n1 'rabbitmqctl list_queues -p '"${rabbitmq_vhost}"
 }
 
