bb3fa92aeab11255435bc10f2c62073b58160c2c
Thierry Marianne <thierry@marianne.io>
2020-02-16 12:12:49 +0100

    Set publication repository

--- src/Twitter/Entity/Publication.php
+++ src/Twitter/Entity/Publication.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Ramsey\Uuid\UuidInterface;
 
 /**
- * @ORM\Entity(repositoryClass="App\Api\Repository\StatusRepository")
+ * @ORM\Entity(repositoryClass="App\Twitter\Entity\PublicationRepository")
  * @ORM\Table(
  *      name="publication",
  *      uniqueConstraints={
---
+++ src/Twitter/Entity/PublicationRepository.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Twitter\Entity;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+/**
+ * @package App\Twitter\Entity
+ */
+class PublicationRepository extends ServiceEntityRepository
+{
+}
