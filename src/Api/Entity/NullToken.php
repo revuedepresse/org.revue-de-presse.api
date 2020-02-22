fc9dbd5e64fc8cba6b4fa4efbe174a27bc42a3d7
Thierry Marianne <thierry@marianne.io>
2020-02-22 22:24:03 +0100

    Introduce token change

--- src/Api/Entity/NullToken.php
+++ src/Api/Entity/NullToken.php
@@ -5,4 +5,35 @@ namespace App\Api\Entity;
 
 class NullToken implements TokenInterface
 {
+    use TokenTrait;
+
+    public function getOAuthToken(): string
+    {
+        return '';
+    }
+
+    public function getOAuthSecret(): string
+    {
+        return '';
+    }
+
+    public function getConsumerKey(): string
+    {
+        return '';
+    }
+
+    public function getConsumerSecret(): string
+    {
+        return '';
+    }
+
+    public function hasConsumerKey(): bool
+    {
+        return false;
+    }
+
+    public static function fromArray(array $token): self
+    {
+        return new self();
+    }
 }
