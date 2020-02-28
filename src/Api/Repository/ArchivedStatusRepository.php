3998addbba1cbfe4cc172f83ad0f260b39b17236
Thierry Marianne <thierry@marianne.io>
2020-02-28 13:57:16 +0100

    Remove deprecated setter

--- src/Api/Repository/ArchivedStatusRepository.php
+++ src/Api/Repository/ArchivedStatusRepository.php
@@ -8,13 +8,11 @@ use App\Api\Entity\ArchivedStatus;
 use App\Api\Entity\Status;
 use App\Api\Exception\InsertDuplicatesException;
 use App\Domain\Repository\StatusRepositoryInterface;
-use App\Domain\Status\Decorator\StatusDecorator;
 use App\Domain\Status\StatusInterface;
 use App\Domain\Status\TaggedStatus;
 use App\Infrastructure\DependencyInjection\MemberRepositoryTrait;
 use App\Infrastructure\DependencyInjection\PublicationPersistenceTrait;
 use App\Infrastructure\DependencyInjection\PublicationRepositoryTrait;
-use App\Infrastructure\DependencyInjection\Status\StatusDecoratorTrait;
 use App\Infrastructure\DependencyInjection\Status\StatusLoggerTrait;
 use App\Infrastructure\DependencyInjection\TaggedStatusRepositoryTrait;
 use App\Infrastructure\DependencyInjection\TimelyStatusRepositoryTrait;
@@ -36,7 +34,6 @@ use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\ORM\NoResultException;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
-use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 use Exception;
 use Psr\Log\LoggerInterface;
@@ -67,8 +64,6 @@ class ArchivedStatusRepository extends ResourceRepository implements
     public Connection $connection;
 
     public bool $shouldExtractProperties;
-
-    protected array $oauthTokens;
 
     /**
      * @param $screenName
@@ -120,113 +115,6 @@ class ArchivedStatusRepository extends ResourceRepository implements
         $totalStatuses = $queryBuilder->getQuery()->getSingleScalarResult();
 
         return (int) $totalStatuses;
-    }
-
-    public function findLatest(
-        int $lastId = null,
-        ?string $aggregateName = null,
-        bool $rawSql = false
-    ) {
-        if ($rawSql) {
-            return $this->findLatestForAggregate($aggregateName);
-        }
-
-        $queryBuilder = $this->selectStatuses();
-
-        if ($lastId !== null) {
-            $queryBuilder->andWhere('t.id < :lastId');
-            $queryBuilder->setParameter('lastId', $lastId);
-        }
-
-        if ($aggregateName !== null) {
-            $queryBuilder->join(
-                't.aggregates',
-                'a'
-            );
-            $queryBuilder->andWhere('a.name = :aggregate_name');
-            $queryBuilder->andWhere('a.screenName IS NOT NULL');
-            $queryBuilder->setParameter('aggregate_name', $aggregateName);
-        }
-
-        $statuses = $queryBuilder->getQuery()->getResult();
-
-        return StatusDecorator::decorateStatus($statuses);
-    }
-
-    public function findLatestForAggregate($aggregateName = null)
-    {
-        $queryTemplate = <<<QUERY
-            SELECT
-            `status`.ust_avatar AS author_avatar,
-            `status`.ust_text AS text,
-            `status`.ust_full_name AS screen_name,
-            `status`.ust_id AS id,
-            `status`.ust_status_id AS status_id,
-            `status`.ust_starred AS starred,
-            `status`.ust_api_document AS original_document,
-            `status`.ust_created_at AS publication_date
-            FROM :timely_status_table `timely_status`, :status_table `status`
-            WHERE `timely_status`.aggregate_name = ':aggregate'
-            AND `timely_status`.status_id = `status`.ust_id
-            ORDER BY `timely_status`.time_range ASC, `timely_status`.publication_date_time DESC
-            LIMIT :max_results
-        ;
-QUERY;
-
-        $query = strtr(
-            $queryTemplate,
-            [
-                ':aggregate'           => $aggregateName,
-                ':max_results'         => 50,
-                ':status_table'        => 'weaving_status',
-                ':timely_status_table' => 'timely_status',
-                ':aggregate_table'     => 'weaving_aggregate',
-            ]
-        );
-
-        $statement = $this->connection->executeQuery($query);
-
-        $statuses = $statement->fetchAll();
-
-        return StatusDecorator::decorateStatus($statuses);
-    }
-
-    public function findLikedStatuses($aggregateName = null)
-    {
-        $queryTemplate = <<<QUERY
-            SELECT
-            `status`.ust_avatar AS author_avatar,
-            `status`.ust_text AS text,
-            `status`.ust_full_name AS screen_name,
-            `status`.ust_id AS id,
-            `status`.ust_status_id AS status_id,
-            `status`.ust_starred AS starred,
-            `status`.ust_api_document AS original_document,
-            `status`.ust_created_at AS publication_date,
-            `liked_status`.liked_by_member_name AS liked_by
-            FROM :liked_status `liked_status`, :status_table `status`
-            WHERE `liked_status`.status_id = `status`.ust_id
-            ORDER BY `liked_status`.time_range ASC, `liked_status`.publication_date_time DESC
-            LIMIT :max_results
-        ;
-QUERY;
-
-        $query = strtr(
-            $queryTemplate,
-            [
-                ':aggregate'       => $aggregateName,
-                ':max_results'     => 500,
-                ':status_table'    => 'weaving_status',
-                ':liked_status'    => 'liked_status',
-                ':aggregate_table' => 'weaving_aggregate',
-            ]
-        );
-
-        $statement = $this->connection->executeQuery($query);
-
-        $statuses = $statement->fetchAll();
-
-        return StatusDecorator::decorateStatus($statuses);
     }
 
     /**
@@ -551,72 +439,7 @@ QUERY;
 
         return $extracts;
     }
-
-    public function setOauthTokens($oauthTokens)
-    {
-        $this->oauthTokens = $oauthTokens;
-
-        return $this;
-    }
-
-    /**
-     * @param array $statuses
-     *
-     * @return mixed
-     */
-    protected function highlightRetweets(array $statuses)
-    {
-        array_walk(
-            $statuses,
-            function (&$status) {
-                $target = sprintf('status of id #%d', (int) $status['id']);
-                if ($status['original_document'] === '') {
-                    DocumentException::throwsEmptyDocumentException((int) $status['id']);
-                }
-
-                $decodedValue  = json_decode(
-                    $status['original_document'],
-                    true,
-                    512,
-                    JSON_THROW_ON_ERROR
-                );
-
-                $lastJsonError = json_last_error();
-
-                if (
-                    $lastJsonError === JSON_ERROR_NONE &&
-                    array_key_exists('retweeted_status', $decodedValue)
-                ) {
-                    $status['text'] = 'RT @' . $decodedValue['retweeted_status']['user']['screen_name'] . ': ' .
-                        $decodedValue['retweeted_status']['full_text'];
-                    return;
-                }
-
-                DocumentException::throwsDecodingException(
-                    $lastJsonError,
-                    (int) $status['id']
-                );
-            }
-        );
-
-        return $statuses;
-    }
-
-    /**
-     * @return QueryBuilder
-     */
-    protected function selectStatuses()
-    {
-        $queryBuilder = $this->timelyStatusRepository->selectStatuses();
-
-        if (!empty($this->oauthTokens)) {
-            $queryBuilder->andWhere('s.identifier IN (:identifier)');
-            $queryBuilder->setParameter('identifier', $this->oauthTokens);
-        }
-
-        return $queryBuilder;
-    }
-
+    
     /**
      * @param EntityManagerInterface $entityManager
      */
@@ -635,9 +458,6 @@ QUERY;
     /**
      * @param                        $likedStatuses
      * @param EntityManagerInterface $entityManager
-     *
-     * @throws ORMException
-     * @throws OptimisticLockException
      */
     private function flushLikedStatuses(
         $likedStatuses,
