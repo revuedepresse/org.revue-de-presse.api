7058563b54ee7f9ebd116d16910e3a92eef8724d
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2020-11-27 12:00:02 +0100

    Remove Doctrine fixtures bundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,7 +7,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     \Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['dev' => true, 'test' => true],
 ];
