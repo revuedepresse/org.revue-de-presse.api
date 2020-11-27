41f6bcce87b5745e03c50ed5b41ab7364b4cc74c
Thierry Marianne <thierrymarianne@users.noreply.github.com>
2020-11-27 12:16:20 +0100

    Fix dependencies configuration (#210)
    Add emoji folder in web to VCS ignore list
    Remove graphs from public directory
    Upgrade base PHP FPM image
    Bump version of apcu
    Move assets from web directory to public directory
    Fix dependencies configuration file (so that it becomes compatible with composer 2.x)
    Revise PHP FPM configuration
    Add emoji directory to VCS ignore list
    Remove web profiler
    Remove Doctrine fixtures bundle
    Remove test class for membership
    Move dependencies around

--- config/bundles.php
+++ config/bundles.php
@@ -7,8 +7,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     \Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['dev' => true, 'test' => true],
 ];
