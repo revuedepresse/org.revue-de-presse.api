283b5e4861d8456b8f704b0cbd7868163d0b76e1
revue-de-presse <contact+github@revue-de-presse.org>
2022-05-28 16:07:15 +0200

    Set file permissions when installing app

--- provisioning/containers/_scripts/install-app-requirements.sh
+++ provisioning/containers/_scripts/install-app-requirements.sh
@@ -74,46 +74,33 @@ function install_app_requirements() {
     echo '--- END ---'
 
     find "${project_dir}"  \
-        -maxdepth 1 \
-        -executable \
-        -readable \
-        -type d \
-        -not \( \
-          -path "${project_dir}"'/.git/**/*' -o \
-          -path "${project_dir}"'/public/.git/**/*' \
-        \) \
-        -exec /bin/bash -c 'export file_path="{}" && \chown --recursive '"${WORKER_UID}"':'"${WORKER_GID}"' "${file_path}"' \; \
-        -exec /bin/bash -c 'export file_path="{}" && \chmod --recursive uog-rwx "${file_path}"' \; \
-        -exec /bin/bash -c 'export file_path="{}" && \chmod --recursive  ug+rx "${file_path}"' \; && \
-        printf '%s.%s' 'Successfully changed directories permissions' $'\n'
+      -maxdepth 1 \
+      -executable \
+      -readable \
+      -type d \
+      -not -path "${project_dir}"'/.git/**/*' \
+      -exec /bin/bash -c 'export file_path="{}" && \chown --recursive '"${WORKER_UID}"':'"${WORKER_GID}"' "${file_path}"' \; \
+      -exec /bin/bash -c 'export file_path="{}" && \chmod --recursive og-rwx "${file_path}"' \; \
+      -exec /bin/bash -c 'export file_path="{}" && \chmod --recursive g+rx "${file_path}"' \; && \
+      printf '%s.%s' 'Successfully changed directories permissions' $'\n'
+
+      find "${project_dir}" \
+      -maxdepth 2 \
+      -executable \
+      -readable \
+      -path "${project_dir}"'/var' \
+      -type d \
+      -exec /bin/bash -c 'export file_path="{}" && \chmod --recursive g+w "${file_path}"' \; && \
+      printf '%s.%s' 'Successfully made var directories writable' $'\n'
 
     find "${project_dir}" \
-        -type f \
-        -readable \
-        -not \( \
-          -path "${project_dir}"'/.git/**/*' -o \
-          -path "${project_dir}"'/public/.git/**/*' \
-        \) \
-        -exec /bin/bash -c 'export file_path="{}" && \chown '"${WORKER_UID}"':'"${WORKER_GID}"' "${file_path}"' \; \
-        -exec /bin/bash -c 'export file_path="{}" && \chmod uog-rwx "${file_path}"' \; \
-        -exec /bin/bash -c 'export file_path="{}" && \chmod  ug+r "${file_path}"' \; && \
-        printf '%s.%s' 'Successfully changed files permissions' $'\n'
-
-    find "${project_dir}" \
-        -executable \
-        -readable \
-        -path "${project_dir}"'/public/wp-content' \
-        -type d \
-        -exec /bin/bash -c 'export file_path="{}" && \chmod --recursive ug+w "${file_path}"' \; && \
-        printf '%s.%s' 'Successfully made wp-content subdirectories writable' $'\n'
-
-    find "${project_dir}" \
-        -executable \
-        -readable \
-        -path "${project_dir}"'/provisioning' \
-        -type d \
-        -exec /bin/bash -c 'export file_path="{}" && \chmod --recursive u+w "${file_path}"' \; && \
-        printf '%s.%s' 'Successfully made provisioning subdirectories writable' $'\n'
+      -type f \
+      -readable \
+      -not -path "${project_dir}"'/.git/**/*' \
+      -exec /bin/bash -c 'export file_path="{}" && \chown '"${WORKER_UID}"':'"${WORKER_GID}"' "${file_path}"' \; \
+      -exec /bin/bash -c 'export file_path="{}" && \chmod og-rwx "${file_path}"' \; \
+      -exec /bin/bash -c 'export file_path="{}" && \chmod g+r "${file_path}"' \; && \
+      printf '%s.%s' 'Successfully changed files permissions' $'\n'
 }
 install_app_requirements
 
