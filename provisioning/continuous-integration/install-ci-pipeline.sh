1eb0b6da4ca995eda286b3bab7c37b4842ff10fd
revue-de-presse <contact+github@revue-de-presse.org>
2022-05-29 12:34:16 +0200

    Revise libsodium version

--- provisioning/continuous-integration/install-ci-pipeline.sh
+++ provisioning/continuous-integration/install-ci-pipeline.sh
@@ -12,15 +12,18 @@ function install_pipeline() {
     /bin/bash -c "${HOME}/.phpenv/versions/$(phpenv version-name)/bin/phpize ."
     /bin/bash -c "./configure --with-php-config=${HOME}/.phpenv/versions/$(phpenv version-name)/bin/php-config"
     make && make install
+    echo 'extension=amqp' > "${HOME}/.phpenv/versions/$(phpenv version-name)/etc/conf.d/amqp.ini"
 
     # [libsodium](https://docs.cloudbees.com/docs/cloudbees-codeship/latest/basic-languages-frameworks/php#_libsodium)
     wget https://raw.githubusercontent.com/codeship/scripts/master/packages/libsodium.sh -O /tmp/libsodium.sh
-    LIBSODIUM_VERSION='2.0.22' bash -c '. /tmp/libsodium.sh'
+    LIBSODIUM_VERSION='1.0.18' bash -c '. /tmp/libsodium.sh'
     LD_LIBRARY_PATH="${HOME}/cache/libsodium/lib PKG_CONFIG_PATH=${HOME}/cache/libsodium/lib/pkgconfig" \
-    LDFLAGS="-L${HOME}/cache/libsodium/lib" pecl install libsodium
+    LDFLAGS="-L${HOME}/cache/libsodium/lib" pecl install -f libsodium
+    echo 'extension=libsodium' > "${HOME}/.phpenv/versions/$(phpenv version-name)/etc/conf.d/libsodium.ini"
 
-    /bin/bash -c "rm -f ${HOME}/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini"
-    echo 'extension=amqp' > "${HOME}/.phpenv/versions/$(phpenv version-name)/etc/conf.d/amqp.ini"
+    local xdebug_configuration_file
+    xdebug_configuration_file="${HOME}/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini"
+    /bin/bash -c "test -e ${xdebug_configuration_file} && rm -f ${xdebug_configuration_file}"
 
     cd ~/src/github.com/thierrymarianne/api.revue-de-presse.org || exit
     cp provisioning/continuous-integration/parameters_test_codeship.yml.dist .env.test
