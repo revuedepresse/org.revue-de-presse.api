a730734d2ee842318ebea4a254a261ba7561f19e
Thierry Marianne <thierry@marianne.io>
2022-06-12 00:52:47 +0200

    Removed twig package

--- config/bundles.php
+++ config/bundles.php
@@ -7,6 +7,4 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['dev' => true, 'test' => true],
 ];
