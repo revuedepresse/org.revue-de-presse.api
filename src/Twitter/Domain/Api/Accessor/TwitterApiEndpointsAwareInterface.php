0f12aa6f31af71c1037591a715f750eb97612496
Thierry Marianne <thierry@marianne.io>
2022-06-14 10:19:15 +0200

    Added endpoint to remove members from a list

--- src/Twitter/Domain/Api/Accessor/TwitterApiEndpointsAwareInterface.php
+++ src/Twitter/Domain/Api/Accessor/TwitterApiEndpointsAwareInterface.php
@@ -6,6 +6,8 @@ interface TwitterApiEndpointsAwareInterface
 {
     public const API_ENDPOINT_MEMBERS_LISTS = '/lists/members/create_all';
 
+    public const API_ENDPOINT_REMOVE_MEMBERS_FROM_LISTS = '/lists/members/destroy_all';
+
     public const API_ENDPOINT_OWNERSHIPS = '/lists/ownerships';
 
     public const API_ENDPOINT_RATE_LIMIT_STATUS = '/application/rate_limit_status';
