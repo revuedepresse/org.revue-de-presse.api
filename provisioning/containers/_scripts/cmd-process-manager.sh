24b1f92aba8b38bbe3c060aac91f810c33b398e1
Thierry Marianne <thierry@marianne.io>
2022-06-16 16:03:09 +0200

    Run process manager in foreground

--- provisioning/containers/_scripts/cmd-process-manager.sh
+++ provisioning/containers/_scripts/cmd-process-manager.sh
@@ -2,20 +2,30 @@
 set -Eeo pipefail
 
 start() {
-    cd "/var/www/${WORKER}" || exit
+    local project_dir
+    project_dir="/var/www/${WORKER}"
+
+    cd "${project_dir}" || exit
 
     export \
       APP_ENV=prod \
       MEMORY_LIMIT=256M \
       MESSAGES=100 \
-      PROJECT_DIR="/var/www/${WORKER}" \
+      PROJECT_DIR="${project_dir}" \
       PROJECT_NAME='wildcard' \
       SYMFONY_ENV=prod \
       TIME_LIMIT=600
 
-    pm2 start bin/consume-fetch-publication-messages.sh \
-      2>> "/var/www/${WORKER}/var/log/${WORKER}.error.log" | \
-      tee --append "/var/www/${WORKER}/var/log/${WORKER}.log"
+    pm2 \
+      --instances 4 \
+      --log-type json \
+      --max-memory-restart 268435456 \
+      --no-daemon \
+      --restart-delay=10000 \
+      start bin/consume-fetch-publication-messages.sh \
+      --name 'Dispatch Fetch member publications AMQP messages' \
+      2>> "${project_dir}/var/log/${WORKER}.error.log" | \
+      tee --append "${project_dir}/var/log/${WORKER}.log"
 }
 start
 
