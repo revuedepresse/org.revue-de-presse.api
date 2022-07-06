e3ecc720f0bd059dd58ea2890c589b0258d56b42
Thierry Marianne <thierry@marianne.io>
2022-07-06 21:59:25 +0200

    Resolved conflicts

--- src/FrenchMediaShareholders/Repository/OwnershipRepository.php
+++ src/FrenchMediaShareholders/Repository/OwnershipRepository.php
@@ -1,12 +1,6 @@
 <?php
 
-<<<<<<<< HEAD:src/PressMedia/Repository/OwnershipRepository.php
-namespace App\PressMedia\Repository;
-|||||||| 3ea9802d:src/App/Media/Repository/OwnershipRepository.php
-namespace App\Media\Repository;
-========
 namespace App\FrenchMediaShareholders\Repository;
->>>>>>>> worker-v3.4.0:src/FrenchMediaShareholders/Repository/OwnershipRepository.php
 
 use Doctrine\ORM\EntityRepository;
 
--- src/Twitter/Infrastructure/Http/Compliance/RateLimitCompliance.php
+++ src/Twitter/Infrastructure/Http/Compliance/RateLimitCompliance.php
@@ -1,47 +1,18 @@
 <?php
 declare(strict_types=1);
 
-<<<<<<<< HEAD:src/Twitter/Infrastructure/Api/Moderator/ApiLimitModerator.php
-namespace App\Twitter\Infrastructure\Api\Moderator;
-|||||||| 3ea9802d:src/WeavingTheWeb/Bundle/ApiBundle/Moderator/ApiLimitModerator.php
-namespace WeavingTheWeb\Bundle\ApiBundle\Moderator;
-========
 namespace App\Twitter\Infrastructure\Http\Compliance;
->>>>>>>> worker-v3.4.0:src/Twitter/Infrastructure/Http/Compliance/RateLimitCompliance.php
 
-<<<<<<<< HEAD:src/Twitter/Infrastructure/Api/Moderator/ApiLimitModerator.php
-use App\Twitter\Infrastructure\Api\Throttling\ApiLimitModeratorInterface;
-|||||||| 3ea9802d:src/WeavingTheWeb/Bundle/ApiBundle/Moderator/ApiLimitModerator.php
-========
 use App\Twitter\Infrastructure\Http\Throttling\RateLimitComplianceInterface;
->>>>>>>> worker-v3.4.0:src/Twitter/Infrastructure/Http/Compliance/RateLimitCompliance.php
 use Psr\Log\LoggerInterface;
 use function strtr;
 
 /**
  * @author revue-de-presse.org <thierrymarianne@users.noreply.github.com>
  */
-<<<<<<<< HEAD:src/Twitter/Infrastructure/Api/Moderator/ApiLimitModerator.php
-class ApiLimitModerator implements ApiLimitModeratorInterface
-|||||||| 3ea9802d:src/WeavingTheWeb/Bundle/ApiBundle/Moderator/ApiLimitModerator.php
-class ApiLimitModerator
-========
 class RateLimitCompliance implements RateLimitComplianceInterface
->>>>>>>> worker-v3.4.0:src/Twitter/Infrastructure/Http/Compliance/RateLimitCompliance.php
 {
-<<<<<<<< HEAD:src/Twitter/Infrastructure/Api/Moderator/ApiLimitModerator.php
-    /**
-     * @var LoggerInterface $logger
-     */
     protected LoggerInterface $logger;
-|||||||| 3ea9802d:src/WeavingTheWeb/Bundle/ApiBundle/Moderator/ApiLimitModerator.php
-    /**
-     * @var \Psr\Log\LoggerInterface $logger
-     */
-    protected $logger;
-========
-    protected LoggerInterface $logger;
->>>>>>>> worker-v3.4.0:src/Twitter/Infrastructure/Http/Compliance/RateLimitCompliance.php
 
     public function __construct(LoggerInterface $logger = null)
     {
