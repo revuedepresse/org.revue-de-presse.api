b18de5ec679f6df40786c425f52a7f7432112686
Thierry Marianne <thierry@marianne.io>
2022-07-07 18:22:09 +0200

    Removed FriendsOfBehatSymfonyExtensionBundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,6 +8,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
-    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
     Kreait\Firebase\Symfony\Bundle\FirebaseBundle::class => ['all' => true],
 ];
