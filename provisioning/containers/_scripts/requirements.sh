bddedeec77c049049526b46f8eb8a64b11020d4f
Thierry Marianne <thierry@marianne.io>
2022-07-08 21:24:03 +0200

    Do not touch volumes permissions if any

--- provisioning/containers/_scripts/requirements.sh
+++ provisioning/containers/_scripts/requirements.sh
@@ -235,6 +235,7 @@ function install_app_requirements() {
         -executable \
         -readable \
         -type d \
+        -not -path "${project_dir}"'/provisioning/volumes' \
         -exec /bin/bash -c 'export file_path="{}" && \chown --recursive '"${SERVICE_OWNER_UID}"':'"${SERVICE_OWNER_GID}"' "${file_path}"' \; \
         -exec /bin/bash -c 'export file_path="{}" && \chmod --recursive og-rwx "${file_path}"' \; \
         -exec /bin/bash -c 'export file_path="{}" && \chmod --recursive g+rx "${file_path}"' \; && \
@@ -252,6 +253,7 @@ function install_app_requirements() {
     find "${project_dir}" \
         -type f \
         -readable \
+        -not -path "${project_dir}"'/provisioning/volumes' \
         -exec /bin/bash -c 'export file_path="{}" && \chown '"${SERVICE_OWNER_UID}"':'"${SERVICE_OWNER_GID}"' "${file_path}"' \; \
         -exec /bin/bash -c 'export file_path="{}" && \chmod og-rwx "${file_path}"' \; \
         -exec /bin/bash -c 'export file_path="{}" && \chmod g+r "${file_path}"' \; && \
@@ -260,6 +262,7 @@ function install_app_requirements() {
     find "${project_dir}"  \
         -type f \
         -not -path "${project_dir}"'/bin' \
+        -not -path "${project_dir}"'/provisioning/volumes' \
         -exec /bin/bash -c 'export file_path="{}" && \chown --recursive '"${SERVICE_OWNER_UID}"':'"${SERVICE_OWNER_GID}"' "${file_path}"' \; \
         -exec /bin/bash -c 'export file_path="{}" && \chmod --recursive ug+x "${file_path}"' \; && \
         printf '%s.%s' 'Successfully changed binaries permissions' $'\n'
