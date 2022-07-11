479c0267bf7737ad723ca5b15bd7cfba28684f9c
Thierry Marianne <thierry@marianne.io>
2022-07-11 17:33:17 +0200

    Renamed WORKER_WORKSPACE env var to WORKER

--- provisioning/containers/_scripts/entrypoint-process-manager.sh
+++ provisioning/containers/_scripts/entrypoint-process-manager.sh
@@ -4,13 +4,13 @@ set -Eeo pipefail
 source '/scripts/requirements.sh'
 
 function dockerize() {
-    create_log_files_when_non_existing "${WORKER}"
+    create_log_files_when_non_existing "${WORKER_DIR}"
 
     local cmd
     cmd="$(cat <<-CMD
     /usr/local/bin/dockerize \
-    -stdout "/var/www/${WORKER}/var/log/${WORKER}.log" \
-    -stderr "/var/www/${WORKER}/var/log/${WORKER}.error.log" \
+    -stdout "/var/www/${WORKER_DIR}/var/log/${WORKER_DIR}.log" \
+    -stderr "/var/www/${WORKER_DIR}/var/log/${WORKER_DIR}.error.log" \
     -timeout 600s \
     /start.sh
 CMD
