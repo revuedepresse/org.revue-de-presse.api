e1d1dfee9c6c5e82103e8acfe791ead0c2cbdedc
Thierry Marianne <thierry@marianne.io>
2023-01-24 19:03:53 +0100

    Adding kreait/firebase-bundle package

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Kreait\Firebase\Symfony\Bundle\FirebaseBundle::class => ['all' => true],
 ];
