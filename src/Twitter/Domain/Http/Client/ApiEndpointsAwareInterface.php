1e485f16f61ff69e44f7cd0b8d73a757e20d5fb1
Thierry Marianne <thierry@marianne.io>
2023-02-02 19:00:28 +0100

    Revising Twitter API endpoint definition

--- src/Twitter/Domain/Http/Client/ApiEndpointsAwareInterface.php
+++ src/Twitter/Domain/Http/Client/ApiEndpointsAwareInterface.php
@@ -6,9 +6,9 @@ interface ApiEndpointsAwareInterface
 {
     public const API_ENDPOINT_GET_MEMBERS_LISTS = '/lists/members';
 
-    public const API_ENDPOINT_MEMBERS_LISTS = '/lists/members/create_all';
+    public const API_ENDPOINT_MEMBERS_LISTS = '/lists/members/create_all?list_id=:list_id';
 
-    public const API_ENDPOINT_MEMBERS_LISTS_VERSION_2 = '/lists/:id/members';
+    public const API_ENDPOINT_MEMBERS_LISTS_VERSION_2 = '/lists/:list_id/members';
 
     public const API_ENDPOINT_REMOVE_MEMBERS_FROM_LISTS = '/lists/members/destroy_all';
 
