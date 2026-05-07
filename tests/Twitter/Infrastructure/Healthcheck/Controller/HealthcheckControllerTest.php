100cdba625696e49f203024fd8e8c52ee247424e
revue-de-presse <contact+github@revue-de-presse.org>
2026-05-07 14:00:33 +0200

    Cover the HealthcheckController
    Signed-off-by: Thierry Marianne <thierry@marianne.io>

---
+++ tests/Twitter/Infrastructure/Healthcheck/Controller/HealthcheckControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Twitter\Infrastructure\Healthcheck\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * @group controller
+ */
+class HealthcheckControllerTest extends WebTestCase
+{
+    public function test_get_returns_empty_json_payload_with_200(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/healthcheck');
+
+        $response = $client->getResponse();
+
+        self::assertSame(200, $response->getStatusCode());
+        self::assertJson($response->getContent());
+        self::assertSame('[]', $response->getContent());
+    }
+
+    public function test_options_preflight_returns_cors_response(): void
+    {
+        $client = static::createClient();
+        $client->request('OPTIONS', '/api/healthcheck');
+
+        self::assertSame(200, $client->getResponse()->getStatusCode());
+    }
+}
