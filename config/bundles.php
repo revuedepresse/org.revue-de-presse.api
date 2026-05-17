9a01c7e6be638c7b90f6feb19e3888c1d2b62932
revue-de-presse <contact+github@revue-de-presse.org>
2026-05-17 17:21:18 +0200

    Add nelmio/cors-bundle ^2.5 with /api/* policy

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
