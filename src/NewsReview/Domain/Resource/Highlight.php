e2ef6882d0c28dd3a4b3c4657351afa1f3e65c96
revue-de-presse <contact+github@revue-de-presse.org>
2026-05-17 17:42:32 +0200

    Add Highlight API Platform resource at GET /api/highlights

---
+++ src/NewsReview/Domain/Resource/Highlight.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\NewsReview\Domain\Resource;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use App\NewsReview\Infrastructure\ApiPlatform\State\HighlightCollectionProvider;
+
+#[ApiResource(
+    shortName: 'Highlight',
+    operations: [
+        new GetCollection(
+            uriTemplate: '/highlights',
+            paginationEnabled: true,
+            paginationItemsPerPage: 25,
+            paginationMaximumItemsPerPage: 100,
+            paginationClientItemsPerPage: true,
+            cacheHeaders: [
+                'max_age'        => 3600,
+                'shared_max_age' => 3600,
+                'public'         => true,
+                'vary'           => ['Accept', 'Authorization'],
+            ],
+            security: 'is_granted("ROLE_USER")',
+            provider: HighlightCollectionProvider::class,
+            output: HighlightDto::class,
+        ),
+    ],
+)]
+final class Highlight
+{
+}
